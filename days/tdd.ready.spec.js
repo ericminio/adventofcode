16d6fe3b538b24e802dfd2135a21c9a496707728
Eric Mignot <eric.mignot@gmail.com>
2015-12-04 13:39:50 -0500

    tdd ready

---
+++ days/tdd.ready.spec.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+
+describe('Testing environment', function () {
+    
+    it('is ready to use chai with mocha', function () {
+        expect(1 + 1).to.equal(2);
+    });
+
+});
