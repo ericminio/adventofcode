ed7661bebca3116a427c40184d3ed97a84e0c7c2
Eric Mignot <eric.mignot@gmail.com>
2015-12-04 14:26:20 -0500

    day 1 - can read address of floor -3 ?

--- days/day.1.spec.js
+++ days/day.1.spec.js
@@ -15,5 +15,9 @@ describe('day 1 challenge', function() {
         it('can read second-order address of floor 1', function() {
             expect(floorOf('()(')).to.equal(1);
         });
+        
+        it('can read address of floor -3', function() {
+            expect(floorOf(')))')).to.equal(-3);
+        });
     });
 });
