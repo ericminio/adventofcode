5289e5e2add7fe6dedc3c40e4f99d97af647a138
Eric Mignot <eric.mignot@gmail.com>
2015-12-04 14:14:27 -0500

    try #3...

--- days/day.1.spec.js
+++ days/day.1.spec.js
@@ -1,7 +1,7 @@
 var expect = require('chai').expect;
 
 var floorOf = function(address) {
-    return address.split('(').length;
+    return address.split('(').length-1;
 };
 
 describe('day 1 challenge', function() {
