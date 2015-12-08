23ba34db23254831a476648bf34719c2e607664f
Eric Mignot <eric.mignot@gmail.com>
2015-12-08 11:56:22 -0500

    day 2 - a little more practice for Stufur

--- days/day.2.spec.js
+++ days/day.2.spec.js
@@ -6,8 +6,7 @@ describe('An elf', function() {
     var stufur = new Elf();
 
     it('knows how much wrapping paper is needed for a given present', function() {
-        var present = { length: 2, width: 3, height: 4 };
-        
-        expect(stufur.paperSurfaceFor(present)).to.equal(52+6);
+        expect(stufur.paperSurfaceFor({ length: 2, width: 3, height: 4 })).to.equal(52+6);
+        expect(stufur.paperSurfaceFor({ length: 1, width: 1, height: 10 })).to.equal(42+1);
     });
 });
