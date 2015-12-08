4535e46fff5549f2790c50504d1713b641dac7b6
Eric Mignot <eric.mignot@gmail.com>
2015-12-08 11:53:12 -0500

    try #1

--- days/elf.js
+++ days/elf.js
@@ -1,7 +1,8 @@
 var Elf = function() {};
 
 Elf.prototype.paperSurfaceFor = function(present) {
-    
+    return 2*present.length*present.width + 2*present.length*present.height + 2*present.width*present.height
+            + Math.min(present.length*present.width, present.length*present.height, present.width*present.height)
 };
 
 module.exports = Elf;
