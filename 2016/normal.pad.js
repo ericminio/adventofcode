1a4b8b28bf61328abebfb70ff41147ed976eddd8
Eric Mignot <eric.mignot@gmail.com>
2016-12-02 09:39:35 -0500

    extract layouts

--- 2016/normal.pad.js
+++ 2016/normal.pad.js
@@ -1,17 +1,16 @@
 module.exports = {
+    layout:[
+        '123',
+        '456',
+        '789'
+    ],
     allows: function(position) {
         return this.digitOf(position) != undefined;
     },
     digitOf: function(position) {
-        var pad = [
-            '123',
-            '456',
-            '789'
-        ];
-        var line = pad[1-position.y];
+        var line = this.layout[1-position.y];
         if (line == undefined) { return undefined; }
         var digit = line[1+position.x];
-        if (digit == undefined || digit == ' ') { return undefined; }
 
         return digit;
     }
--- 2016/star.pad.js
+++ 2016/star.pad.js
@@ -1,19 +1,20 @@
 module.exports = {
+    layout: [
+        '  1  ',
+        ' 234 ',
+        '56789',
+        ' ABC ',
+        '  D  '
+    ],
+
     allows: function(position) {
         return this.digitOf(position) != undefined;
     },
     digitOf: function(position) {
-        var pad = [
-            '  1  ',
-            ' 234 ',
-            '56789',
-            ' ABC ',
-            '  D  '
-        ];
-        var line = pad[2-position.y];
+        var line = this.layout[2-position.y];
         if (line == undefined) { return undefined; }
         var digit = line[position.x];
-        if (digit == undefined || digit == ' ') { return undefined; }
+        if (digit == ' ') { return undefined; }
 
         return digit;
     }
