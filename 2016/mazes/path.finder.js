d18633639ed336517134d2f435c9d5b5919186a8
Eric Mignot <eric.mignot@gmail.com>
2016-12-14 14:18:33 -0500

    we now can find our way in a maze...

--- 2016/mazes/path.finder.js
+++ 2016/mazes/path.finder.js
@@ -11,28 +11,68 @@ PathFinder.prototype.startAt = function(location) {
 };
 PathFinder.prototype.pathTo = function(location) {
     this.target = location;
-
+    var tree = leafs = [this.start];
+    var found = undefined;
+    while (!found) {
+        leafs = this.nextGeneration(leafs, tree);
+        tree = tree.concat(leafs);
+        found = this.isTargetReached(leafs);
+    }
+    this.leafs = leafs;
+    return this.buildPath(found);
+};
+PathFinder.prototype.buildPath = function(target) {
     var path = [];
-    path.push(this.start);
-
-    candidates = this.children(this.start);
-    candidates.forEach(function(candidate) {
-        if (self.targetReached(candidate) ) { path.push(candidate); }
-    })
-
+    var current = target;
+    while (current != undefined) {
+        path.push(current);
+        current = current.parent;
+    }
     return path;
 };
-PathFinder.prototype.children = function(location) {
+PathFinder.prototype.children = function(location, alreadyIdentified) {
     var candidates = [ north(location), east(location), south(location), west(location) ];
     var children = [];
     candidates.forEach(function(candidate) {
-        if (!self.maze.isWall(candidate)) { children.push(candidate); }
+        if (!self.maze.isWall(candidate)) {
+            children.push(candidate);
+            candidate.parent = location;
+        }
     });
 
-    return children;
+    return this.exclude(alreadyIdentified, children);
+};
+PathFinder.prototype.nextGeneration = function(leafs, tree) {
+    var next = [];
+    for (var i=0; i<leafs.length; i++) {
+        var children = this.children(leafs[i], next);
+        next = next.concat(this.exclude(tree, children));
+    }
+    return next;
+};
+PathFinder.prototype.exclude = function(tree, children) {
+    var keep = [];
+    for (var i=0; i<children.length; i++) {
+        var child = children[i];
+        var visited = false;
+        for (var j=0; j<tree.length; j++) {
+            var node = tree[j];
+            if (node.x == child.x && node.y == child.y) { visited = true; break; }
+        }
+        if (!visited) { keep.push(child); }
+    }
+    return keep;
 };
 PathFinder.prototype.targetReached = function(location) {
     return location.x == this.target.x && location.y == this.target.y;
+};
+PathFinder.prototype.isTargetReached = function(leafs) {
+    for (var i=0; i<leafs.length ; i++) {
+        var leaf = leafs[i];
+        if (this.targetReached(leaf) ) {
+            return leaf;
+        }
+    };
 };
 
 module.exports = PathFinder;
--- 2016/mazes/path.finder.spec.js
+++ 2016/mazes/path.finder.spec.js
@@ -16,7 +16,8 @@ describe('PathFinder', function() {
                 '...'
             ],
             isWall: function(location) {
-                if (location.x<0 || location.y<0) { return false; }
+                if (location.x<0 || location.y<0) { return true; }
+                if (location.x>2 || location.y>2) { return true; }
                 return this.map[location.y][location.x] == '#';
             }
         };
@@ -28,10 +29,13 @@ describe('PathFinder', function() {
         pathFinder.startAt(xy(1, 1));
     });
 
-    it('konws the children of a given location in the maze', function() {
-        expect(pathFinder.children(xy(1, 1))).to.deep.equal([
-            xy(2, 1), xy(1, 2), xy(0, 1)
-        ]);
+    it('knows the children of a given location in the maze', function() {
+        var location = xy(1, 1);
+        var east = xy(2, 1), south = xy(1, 2), west = xy(0, 1);
+        east.parent = location;
+        south.parent = location;
+        west.parent = location;
+        expect(pathFinder.children(location, [])).to.deep.equal([east, south, west ]);
     });
 
     it('can find target east', function() {
@@ -51,5 +55,48 @@ describe('PathFinder', function() {
             '.O',
             '.O'
         ]);
+    });
+
+    it('can find target south-east', function() {
+        var path = pathFinder.pathTo(xy(2, 2));
+
+        expect(renderer.render(path)).to.deep.equal([
+            '.#.',
+            '.OO',
+            '..O'
+        ]);
+    });
+
+    it('can find an hidden target', function() {
+        pathFinder.startAt(xy(0, 0));
+        var path = pathFinder.pathTo(xy(2, 0));
+
+        expect(renderer.render(path)).to.deep.equal([
+            'O#O',
+            'OOO'
+        ]);
+    });
+
+    describe('Next generation', function() {
+
+        it('does not walk back', function() {
+            pathFinder.startAt(xy(1, 1));
+            var path = pathFinder.pathTo(xy(2, 2));
+
+            var startFound = false;
+            var leafs = pathFinder.leafs;
+            for (var i=0; i<leafs.length; i++) {
+                var leaf = leafs[i];
+                if (leaf.x == 1 && leaf.y == 1) { startFound = true; }
+            }
+            expect(startFound).to.equal(false);
+        });
+
+        it('does not visit a leaf twice', function() {
+            pathFinder.startAt(xy(1, 1));
+            var path = pathFinder.pathTo(xy(2, 2));
+
+            expect(pathFinder.leafs.length).to.equal(4);
+        });
     });
 });
