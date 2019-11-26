1638583189cbd84bbe77dff4eeb141c78346b2eb
Mignot <mignoter@corp.advancedsolutions.eds.com>
2019-11-26 09:08:20 -0800

    exploring day.23.part.2

--- 2018/day.23/day.23.spec.js
+++ 2018/day.23/day.23.spec.js
@@ -3,7 +3,14 @@ const puzzle = require('../puzzle.input')
 const {
     parse,
     strongest,
-    inRange
+    inRange,
+    nanobotsInRange,
+    rectangle,
+    doIntersect,
+    weakest,
+    isolated,
+    isInRange,
+    manhatan
 } = require('./lib')
 
 describe('day 23 challenge', ()=> {
@@ -11,7 +18,7 @@ describe('day 23 challenge', ()=> {
 	describe('part 1', ()=>{
 
 		it('has an example', ()=>{
-			var lines = puzzle.lines('day.23', 'example.txt')
+			var lines = puzzle.lines('day.23', 'example-1.txt')
             var nanobots = parse(lines)
             var nanobot = strongest(nanobots)
             expect(nanobot).to.deep.equal({ x:0, y:0, z:0, r:4 })
@@ -28,6 +35,66 @@ describe('day 23 challenge', ()=> {
 
             var count = inRange(nanobot, nanobots)
             expect(count).to.equal(309)
+		})
+	})
+
+    describe('part 2', ()=>{
+
+        var remove = (nanobot, collection)=> {
+            var cleaned = []
+            for (var i=0; i<collection.length;i++) {
+                var candidate = collection[i]
+                if (candidate.x != nanobot.x || candidate.y != nanobot.y || candidate.z != nanobot.z) {
+                    cleaned.push(candidate)
+                }
+            }
+            return cleaned
+        }
+
+		it('exploration', ()=>{
+			var lines = puzzle.lines('day.23', 'input.txt')
+            var nanobots = parse(lines)
+            var boss, next = nanobots
+
+            while (next.length > 2) {
+                boss = strongest(next)
+                next = nanobotsInRange(boss, next)
+                next = remove(boss, next)
+            }
+            console.log(next);
+            console.log(doIntersect(next[0], next[1]));
+
+            var nanobot = next[0]
+            var master = next[1]
+
+            var radius = nanobot.r
+            var offsets = [
+                { dx:+1, dy:+1 },
+                { dx:-1, dy:+1 },
+                { dx:+1, dy:-1 },
+                { dx:-1, dy:-1 }
+            ]
+            var candidates = []
+            var min = manhatan({ x:0, y:0, z:0 }, nanobot)
+            for (var i=0; i<=radius; i++) {
+                for (var j=0; j<offsets.length; j++) {
+                    var offset = offsets[j]
+                    var candidate = {
+                        x: nanobot.x +  i * offset.dx,
+                        y: nanobot.y + (radius - i) * offset.dy,
+                        z: nanobot.z
+                    }
+                    if (isInRange(candidate, master)) {
+                        var distance = manhatan({ x:0, y:0, z: 0}, candidate)
+                        if (distance < min) {
+                            candidates.push(candidate)
+                            min = distance
+                        }
+                    }
+                }
+            }
+            console.log(candidates.length, candidates[candidates.length-1], min);
+
 		})
 	})
 
--- 2018/day.23/lib.js
+++ 2018/day.23/lib.js
@@ -27,13 +27,40 @@ var strongest = (nanobots)=>{
     }
     return best
 }
-
+var weakest = (nanobots)=>{
+    var min = nanobots[0].r
+    var best
+    for (var i=0; i<nanobots.length;i++) {
+        var nanobot = nanobots[i]
+        if (nanobot.r < min) {
+            best = nanobot
+            min = nanobot.r
+        }
+    }
+    return best
+}
+var isolated = (nanobots)=>{
+    var min = inRange(nanobots[0], nanobots)
+    var alones = []
+    for (var i=0; i<nanobots.length;i++) {
+        var nanobot = nanobots[i]
+        var candidate = inRange(nanobot, nanobots)
+        if (candidate  == 1) {
+            alones.push(nanobot)
+        }
+    }
+    return alones
+}
 var manhatan = (n1, n2)=> {
     return Math.abs(n1.x - n2.x) + Math.abs(n1.y - n2.y) + Math.abs(n1.z - n2.z)
 }
 var isInRange = (point, nanobot)=> {
     var distance = manhatan(point, nanobot)
     return distance <= nanobot.r
+}
+var doIntersect = (n1 ,n2)=> {
+    var distance = manhatan(n1, n2)
+    return distance <= (n1.r + n2.r)
 }
 
 var inRange = (nanobot, nanobots)=> {
@@ -46,9 +73,42 @@ var inRange = (nanobot, nanobots)=> {
     }
     return count
 }
+var nanobotsInRange = (nanobot, nanobots)=> {
+    var collection = []
+    for (var i=0; i<nanobots.length;i++) {
+        var candidate = nanobots[i]
+        if (isInRange(candidate, nanobot)) {
+            collection.push(candidate)
+        }
+    }
+    return collection
+}
+
+var rectangle = function(points) {
+    var max = { x:0, y:0, z:0 }
+    points.forEach((point)=>{
+        if (point.x > max.x) { max.x = point.x }
+        if (point.y > max.y) { max.y = point.y }
+        if (point.z > max.z) { max.z = point.z }
+    })
+    var min = { x:max.x, y:max.y, z:max.z }
+    points.forEach((point)=>{
+        if (point.x < min.x) { min.x = point.x }
+        if (point.y < min.y) { min.y = point.y }
+        if (point.z < min.z) { min.z = point.z }
+    })
+    return { min:min, max:max }
+}
 
 module.exports = {
     parse:parse,
 	strongest:strongest,
-    inRange:inRange
+    inRange:inRange,
+    rectangle:rectangle,
+    doIntersect:doIntersect,
+    weakest:weakest,
+    isolated:isolated,
+    nanobotsInRange:nanobotsInRange,
+    isInRange:isInRange,
+    manhatan:manhatan
 }
