00e65838c26cbe83d995c4f5285fb671cd0f5b25
ericminio <eric.mignot@gmail.com>
2021-12-01 07:29:18 -0800

    window size

--- 2021/day.1/day.1.spec.js
+++ 2021/day.1/day.1.spec.js
@@ -5,44 +5,35 @@ describe('day 1 challenge', ()=> {
 
     it('includes an example for part 1', ()=>{ 
         let input = puzzle('day.1', 'example.txt').map(s => parseInt(s));           
-        expect(countIncreasing(input)).to.equal(7);
+        expect(countIncreasingWindow(input, 1)).to.equal(7);
     });    
-    const countIncreasing = (input) => {
-        let count = 0;
-        let previous = input[0];
-        for (var i=1; i<input.length; i++) {
-            if (input[i] > previous) {
-                count ++;
-            }
-            previous = input[i];
-        }
-        return count;
-    }
     it('includes part 1', ()=>{ 
         let input = puzzle('day.1', 'input.txt').map(s => parseInt(s));           
-        expect(countIncreasing(input)).to.equal(1400);
+        expect(countIncreasingWindow(input, 1)).to.equal(1400);
     });
 
     it('includes an example for part 2', ()=>{ 
         let input = puzzle('day.1', 'example.txt').map(s => parseInt(s));           
-        expect(countIncreasingWindow(input)).to.equal(5);
+        expect(countIncreasingWindow(input, 3)).to.equal(5);
     }); 
-    const countIncreasingWindow = (input) => {
+    const countIncreasingWindow = (input, windowSize) => {
         let count = 0;
-
-        let previousSum = input[0] + input[1] + input[2];
+        let previousSum = 0;
+        for (var i=0; i<windowSize; i++) {
+            previousSum += input[i];
+        }
         let sum = previousSum;
-
-        for (var i=3; i<input.length; i++) {
+        for (var i=windowSize; i<input.length; i++) {
             previousSum = sum;
-            sum = previousSum + input[i] - input[i-3]
+            sum += input[i];
+            sum -= input[i-windowSize];
             if (sum > previousSum) { count ++ }
         }
         return count;
     }
     it('includes part 2', ()=>{ 
         let input = puzzle('day.1', 'input.txt').map(s => parseInt(s));           
-        expect(countIncreasingWindow(input)).to.equal(1429);
+        expect(countIncreasingWindow(input, 3)).to.equal(1429);
     });
 
 })
