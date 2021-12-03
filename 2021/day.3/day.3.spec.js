f8f1ac72309d1b8bdada33f72d9ee0a78268b317
ericminio <eric.mignot@gmail.com>
2021-12-03 15:53:43 -0800

    part 1

--- 2021/day.3/day.3.spec.js
+++ 2021/day.3/day.3.spec.js
@@ -1,16 +1,51 @@
 const { expect } = require('chai')
-const { data } = require('../puzzle.input')
+const { lines } = require('../puzzle.input')
 
 describe('day 3 challenge', ()=> {
 
-    const example = data('day.3', 'example.txt');
+    const example = lines('day.3', 'example.txt');
+    const input = lines('day.3', 'input.txt');
     const gamaRateOf = (lines) => {
-        let count = { '0':0, '1':0 }
+        let size = lines[0].length;
+        let count = [];
+        for (var i=0; i< size; i++) {
+            count.push({ '0':0, '1':0 })
+        }
         lines.forEach((line) => {
-            count[line[0]] ++;
+            for (var i=0; i<size; i++) {
+                count[i][line[i]] ++;
+            }
+            
         })
-        return count['0'] > count['1'] ? '0' : '1'
+        let value = '';
+        for (var i=0; i< size; i++) {
+            let digit = count[i]['0'] > count[i]['1'] ? '0' : '1'
+            value += digit;
+        }
+        return value;
     } 
+    const epsilonRateOf = (lines) => {
+        let size = lines[0].length;
+        let count = [];
+        for (var i=0; i< size; i++) {
+            count.push({ '0':0, '1':0 })
+        }
+        lines.forEach((line) => {
+            for (var i=0; i<size; i++) {
+                count[i][line[i]] ++;
+            }
+            
+        })
+        let value = '';
+        for (var i=0; i< size; i++) {
+            let digit = count[i]['0'] < count[i]['1'] ? '0' : '1'
+            value += digit;
+        }
+        return value;
+    } 
+    const powerConsumption = (input) => {
+        return parseInt(gamaRateOf(input), 2) * parseInt(epsilonRateOf(input), 2);
+    }
 
     describe('gamma rate', () => {
 
@@ -20,6 +55,29 @@ describe('day 3 challenge', ()=> {
 
         it('keeps the most common value', () => {
             expect(gamaRateOf(['1', '0', '0'])).to.equal('0');
+            expect(gamaRateOf(['1', '0', '0', '1', '1'])).to.equal('1');
         })
+
+        it('works with 2 digits', () => {
+            expect(gamaRateOf(['10', '01', '01'])).to.equal('01');
+        });
+
+        it('works with the example of part 1', () => {
+            expect(parseInt(gamaRateOf(example), 2)).to.equal(22);
+        });
+    })
+
+    describe('epsilon rate', () => {
+
+        it('works with the example of part 1', () => {
+            expect(parseInt(epsilonRateOf(example), 2)).to.equal(9);
+        });
+    })
+
+    it('gives answer for the example', () => {
+        expect(powerConsumption(example)).to.equal(198);
+    })
+    it('gives answer for the part 1', () => {
+        expect(powerConsumption(input)).to.equal(738234);
     })
 })
