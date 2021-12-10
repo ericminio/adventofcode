62dc2d6a86fd48b1c034e73b57f0684103b1fc99
ericminio <eric.mignot@gmail.com>
2021-12-10 09:46:24 -0800

    day.10 recursive

--- 2021/day.10/day.10.spec.js
+++ 2021/day.10/day.10.spec.js
@@ -1,37 +1,37 @@
 const { expect } = require('chai')
 const { lines } = require('../puzzle.input')
 
-describe.only('day 10 challenge', ()=> {
+describe('day 10 challenge', ()=> {
 
-    it('is about corrupted chunks', () => {
+    it('is about corrupted chunks', () => {
         expect(isCorrupted('<>')).to.equal(false);
     })
 
-    describe('corruption', () => {
+    describe('corruption', () => {
 
-        it('happens with wrong closing', () => {
+        it('happens with wrong closing', () => {
             expect(isCorrupted('<)')).to.equal(')');
             expect(isCorrupted('(>')).to.equal('>');
         })
-        it('does not happen with expected closing', () => {
+        it('does not happen with expected closing', () => {
             expect(isCorrupted('()')).to.equal(false);
         })
-        it('does not happen when sequence is incomplete', () => {
+        it('does not happen when sequence is incomplete', () => {
             expect(isCorrupted('(')).to.equal(false);
         })
-        it('can happen after one correct chunk', () => {
+        it('can happen after one correct chunk', () => {
             expect(isCorrupted('<><)')).to.equal(')');
         })
-        it('can happen after two correct chunks', () => {
+        it('can happen after two correct chunks', () => {
             expect(isCorrupted('<>()(>')).to.equal('>');
         })
-        it('can happen with nested chunks', () => {
+        it('can happen with nested chunks', () => {
             expect(isCorrupted('<(>>')).to.equal('>');
         })
-        it('resists digits that are not tags', () => {
+        it('resists digits that are not tags', () => {
             expect(isCorrupted('<(.)>')).to.equal(false);
         })
-        it('resists example', () => {
+        it('resists example', () => {
             const example = lines('day.10', 'example.txt');
             const result = example.map(line => isCorrupted(line));
 
@@ -40,7 +40,7 @@ describe.only('day 10 challenge', ()=> {
             let score = result.filter(r => r != false).reduce((acc, curr)=> acc += syntaxErrorValueOf(curr), 0);
             expect(score).to.equal(26397);
         })
-        it('helps to solve part 1', () => {
+        it('helps to solve part 1', () => {
             const input = lines('day.10', 'input.txt');
             const result = input.map(line => isCorrupted(line));
             let score = result.filter(r => r != false).reduce((acc, curr)=> acc += syntaxErrorValueOf(curr), 0);
@@ -48,20 +48,17 @@ describe.only('day 10 challenge', ()=> {
             expect(score).to.equal(240123);
         })
     })
-    const isCorrupted = (line) => {
-        let expectedClosing = []
-        for (var i=0; i<line.length; i++) {
-            let tag = line[i];
-            if (isOpening(tag)) {
-                expectedClosing.push(closingOf(tag))
-            }
-            if (isClosing(tag)) {
-                let expected = expectedClosing.pop();
-                if (tag != expected) { return tag; }
-            }
-        }
+    const isCorrupted = (line) => {        
+        return isCorruptedDigit(line, 0, [])
+    }
+    const isCorruptedDigit = (line, index, expectedClosing) => {
+        if (index == line.length) { return false; }
         
-        return false;
+        let tag = line[index];
+        if (isOpening(tag)) { expectedClosing.push(closingOf(tag)) }
+        if (isClosing(tag) && tag != expectedClosing.pop()) { return tag; }
+        
+        return isCorruptedDigit(line, index + 1, expectedClosing);
     }
     const tags = [
         { opening:'<', closing:'>', syntaxErrorValue:25137, completionValue:4 },
@@ -69,29 +66,29 @@ describe.only('day 10 challenge', ()=> {
         { opening:'{', closing:'}', syntaxErrorValue:1197,  completionValue:3 },
         { opening:'[', closing:']', syntaxErrorValue:57,    completionValue:2 },
     ]
-    const isClosing = (tag) => {
+    const isClosing = (tag) => {
         return tags.find(t => t.closing == tag)
     }
-    const isOpening = (tag) => {
+    const isOpening = (tag) => {
         return tags.find(t => t.opening == tag)
     }
-    const closingOf = (tag) => {
+    const closingOf = (tag) => {
         return tags.find(t => t.opening == tag).closing
     }
-    const syntaxErrorValueOf = (tag) => {
+    const syntaxErrorValueOf = (tag) => {
         return tags.find(t => t.closing == tag).syntaxErrorValue
     }
-    const completionValueOf = (tag) => {
+    const completionValueOf = (tag) => {
         return tags.find(t => t.closing == tag).completionValue
     }
     
-    describe('part 2', () => {
+    describe('part 2', () => {
 
-        it('is about completing the incomplete chunks', () => {
+        it('is about completing the incomplete chunks', () => {
             expect(autocomplete('[({(<(())[]>[[{[]{<()<>>')).to.equal('}}]])})]');
         })
 
-        const autocomplete = (line) => {
+        const autocomplete = (line) => {
             let expectedClosing = []
             for (var i=0; i<line.length; i++) {
                 let tag = line[i];
@@ -106,7 +103,7 @@ describe.only('day 10 challenge', ()=> {
             return expectedClosing.reverse().join('') 
         }
 
-        it('has an example', () => {
+        it('has an example', () => {
             const example = lines('day.10', 'example.txt');
             const result = example.filter(line => !isCorrupted(line)).map(line => autocomplete(line));
 
@@ -124,7 +121,7 @@ describe.only('day 10 challenge', ()=> {
             expect(solution(example)).to.equal(288957)
         })
 
-        const solution = (lines) => {
+        const solution = (lines) => {
             const result = lines.filter(line => !isCorrupted(line)).map(line => autocomplete(line));
 
             const scores = result.map(completion => completion.split('').reduce(
@@ -135,7 +132,7 @@ describe.only('day 10 challenge', ()=> {
             return scores[(scores.length-1)/2]
         }
 
-        it('is now solved', () => {
+        it('is now solved', () => {
             const input = lines('day.10', 'input.txt');
             expect(solution(input)).to.equal(3260812321)
         })        
