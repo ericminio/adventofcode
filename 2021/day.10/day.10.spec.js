cd3e4b8800a10b09e3773dd23168d3373d1f8a95
ericminio <eric.mignot@gmail.com>
2021-12-09 21:30:58 -0800

    about corruption

--- 2021/day.10/day.10.spec.js
+++ 2021/day.10/day.10.spec.js
@@ -1,6 +1,6 @@
 const { expect } = require('chai')
 
-describe('day 10 challenge', ()=> {
+describe.only('day 10 challenge', ()=> {
 
     it('is about corrupted chunks', () => {
         expect(isCorrupted('<>')).to.equal(false);
@@ -11,13 +11,31 @@ describe('day 10 challenge', ()=> {
         it('happens with wrong closing', () => {
             expect(isCorrupted('<)')).to.equal(true);
         })
+        it('does not happen with expected closing', () => {
+            expect(isCorrupted('()')).to.equal(false);
+        })
     })
-
     const isCorrupted = (line) => {
-        if (line[1] != '>') {
-            return true;
+        let expectedClosing = []
+        if (isOpening(line[0])) {
+            expectedClosing.push(closingOf(line[0]))
+        }
+        
+        if (isClosing(line[1])) {
+            return line[1] != expectedClosing[0];
         }
         return false;
+    }
+    const isClosing = (tag) => {
+        return tag == '>' || tag == ')'
+    }
+    const isOpening = (tag) => {
+        return tag == '<' || tag == '('
+    }
+    const closingOf = (tag) => {
+        if (tag == '<') return '>';
+        if (tag == '(') return ')';
+        throw new Error('unknown tag ' + tag);
     }
     
 })
