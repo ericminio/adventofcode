a2bd72f08d74137fd0ceb07faa473734732067db
ericminio <eric.mignot@gmail.com>
2022-07-02 09:06:28 -0700

    parsing ready

--- 2021/day.25/faster.spec.js
+++ 2021/day.25/faster.spec.js
@@ -3,11 +3,11 @@ const deepEqual = require('deep-equal');
 const fs = require('fs');
 const path = require('path');
 
-describe.only('day 25 challenge', () => {
+const EMPTY = 0;
+const RIGHT = 1;
+const DOWN = 2;
 
-    const EMPTY = 0;
-    const RIGHT = 1;
-    const DOWN = 2;
+describe.only('day 25 challenge', () => {
 
     it('has an example', () => {
         let start = parse(fs.readFileSync(path.join(__dirname, 'example.txt')).toString());
@@ -15,19 +15,45 @@ describe.only('day 25 challenge', () => {
 
         expect(count).to.equal(58);
     });
+
+    describe('parsing', () => {
+        let pretend = {
+            input: `
+                >.v
+                .v>
+            `,
+            model: [
+                [RIGHT, EMPTY, DOWN],
+                [EMPTY, DOWN, RIGHT]
+            ]
+        };
+
+        it('parses as expected', () => {
+            expect(parse(pretend.input)).to.deep.equal(pretend.model);
+        });
+    });
 });
 
-const moveCountUntilImmobility = (start, listener) => {
+const moveCountUntilImmobility = (map, listener) => {
+    let rowCount = map.length;
+    let columnCount = map[0].length;
     let count = 0;
     let hasMoved = true;
     while (hasMoved && count < 58) {
         hasMoved = false;
+        
         hasMoved = true;
+        
         count ++;
     }
     return count;
 };
 
 const parse = (input) => {    
-    
+    return input.trim().split('\n')
+        .map(line => line.trim().split('').map(char => {
+            if (char == '.') return EMPTY;
+            if (char == '>') return RIGHT;
+            if (char == 'v') return DOWN;
+        }));
 };
