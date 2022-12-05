fcee626969b1388800fe7e63c3d38c4de4da5135
ericminio <emignot@change.org>
2022-12-05 11:44:09 -0800

    ttt

--- 2022/day.1/solve.js
+++ 2022/day.1/solve.js
@@ -3,9 +3,10 @@ const input = (file) => fs.readFileSync(file).toString();
 const groups = (file) => input(file).split(/\n\n/).map(items => items.split('\n'));
 const numberOrZero = (item => Number.isNaN(parseInt(item)) ? 0 : parseInt(item));
 const groupsOfNumbers = (file) => groups(file).map(items => items.map(item => numberOrZero(item)));
+const total = (array) => array.reduce((acc, current) => acc += current, 0);
 
 const elfTotals = (groups) => {
-    return groups.map(group => group.reduce((acc, current) => acc += current, 0));
+    return groups.map(group => total(group));
 };
 
 const solve1 = (file) => {
