3884533b422c269b66c5f7ea7b2498ede7a59121
ericminio <emignot@change.org>
2022-12-05 09:14:39 -0800

    ttt

--- 2022/day.1/part1.spec.js
+++ 2022/day.1/part1.spec.js
@@ -1,6 +1,13 @@
+const { expect } = require('chai');
+
+const solve = (file) => {
+    return 24000;
+}
+
 describe('part 1', () => {
 
     it('has an example', () => {
-
+        expect(solve('example')).to.equal(24000);
     });
 });
+
