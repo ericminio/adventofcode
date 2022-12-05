7ce72b02dc53e8639bd56ea7ce1e02ece82ab887
ericminio <emignot@change.org>
2022-12-05 09:59:46 -0800

    ttt

--- 2022/day.1/part2.spec.js
+++ 2022/day.1/part2.spec.js
@@ -1,24 +1,10 @@
 const { expect } = require('chai');
 const { solve } = require('./solve');
 
-describe('part 1', () => {
+describe('part 2', () => {
 
     it('has an example', () => {
         expect(solve('example')).to.equal(24000);
-    });
-
-    it('works with one elf carrying a single item', () => {
-        expect(solve('one')).to.equal(1000);
-    });
-    it('works with one elf carrying two items', () => {
-        expect(solve('two')).to.equal(3000);
-    });
-    it('works with two elves carrying several items', () => {
-        expect(solve('three')).to.equal(4000);
-    });
-
-    it('is solved', () => {
-        expect(solve('input')).to.equal(72718);
     });
 });
 
