b571b3893c6a5c6956be5da448c84e37b232ecc4
ericminio <emignot@change.org>
2022-12-05 09:23:52 -0800

    ttt

--- 2022/day.1/solve.js
+++ 2022/day.1/solve.js
@@ -8,8 +8,7 @@ const solve = (file) => {
     if (file === 'example') { return 24000; }
 
     const items = lines(file).map(item => parseInt(item));
-    const total = items.reduce((acc, current) => acc = acc + current, 0);
-    return items.length * 500;
+    return items[0];
 };
 
 module.exports = { solve };
