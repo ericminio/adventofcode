284d8f4775ad23d20fb74d5bcfbc56ad72982f0f
ericminio <emignot@change.org>
2022-12-05 09:33:08 -0800

    ttt

--- 2022/day.1/solve.js
+++ 2022/day.1/solve.js
@@ -2,12 +2,13 @@ const fs = require('fs');
 const lines = (file) =>
     fs.readFileSync(`2022/day.1/${file}.txt`)
         .toString()
-        .split('\n');
+        .split('\n')
+        .map(item => parseInt(item));
 
 const solve = (file) => {
     if (file === 'example') { return 24000; }
 
-    const items = lines(file).map(item => Number.isNaN(item) ? 0 : parseInt(item));
+    const items = lines(file);
     items.reduce((acc, current) => console.log(current), 0);
     let total = items[0];
     if (items.length > 2) { total += items[1] }
