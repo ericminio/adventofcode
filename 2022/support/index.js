aa8884a4e71c660f125581ae5a4c6ca0b1d1cbf3
ericminio <eric.mignot@gmail.com>
2022-12-05 22:00:33 -0800

    ttt

--- 2022/support/index.js
+++ 2022/support/index.js
@@ -10,11 +10,11 @@ const orderDescending = (array) => { array.sort((a, b) => b - a); }
 const lines = (file) => input(file).split(/\n/);
 const rounds = (file) => lines(file).map(line => ({ line:line, opponent:line.charAt(0), me:line.charAt(2) }));
 const opponentScore = { 'A':1, 'B':2, 'C': 3 };
-const shapeScore = { 'X':1, 'Y':2, 'Z': 3 };
+const myScore = { 'X':1, 'Y':2, 'Z': 3 };
 const outcomes = { 'A X':3, 'B Y':3, 'C Z':3, 'B X':0, 'C X':6, 'A Y':6, 'C Y':0, 'A Z':0, 'B Z':6 }
 const selectionScore = (file) => rounds(file).map(round => ({
     ...round, 
-    myShapeScore:shapeScore[round.me],
+    myShapeScore:myScore[round.me],
     opponentShapeScore: opponentScore[round.opponent],
 }));
 const outcome = (file) => selectionScore(file).map(round => ({
