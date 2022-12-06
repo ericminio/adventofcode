a9cde194fef6a6d5c6461a8a4d7040522b17cd21
ericminio <emignot@change.org>
2022-12-06 09:12:37 -0800

    ttt

--- 2022/day.3/solve.js
+++ 2022/day.3/solve.js
@@ -2,7 +2,7 @@ const { total } = require('../support');
 
 const adjust = (code) => code > 94 ? code - 96 : code - 64 + 26;
 const priorityOf = (letter) => adjust(letter.charCodeAt(0));
-const priorities = (file) => [priorityOf('p'), priorityOf('L'), 42, priorityOf('v'), priorityOf('t'), priorityOf('s')];
+const priorities = (file) => [priorityOf('p'), priorityOf('L'), priorityOf('P'), priorityOf('v'), priorityOf('t'), priorityOf('s')];
 
 const solve1 = (file) => {
     return total(priorities(file));
