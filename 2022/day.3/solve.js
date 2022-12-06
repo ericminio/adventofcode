8eed6f56b3fef04eba43c285370a54f39ee2f914
ericminio <emignot@change.org>
2022-12-06 09:43:21 -0800

    ttt

--- 2022/day.3/solve.js
+++ 2022/day.3/solve.js
@@ -1,10 +1,10 @@
-const { total } = require('../support');
+const { lines, total } = require('../support');
 
 const adjust = (code) => code > 94 ? code - 96 : code - 64 + 26;
 const priorityOf = (letter) => adjust(letter.charCodeAt(0));
 const duplicateIn = (one, two) => { return one.filter(value => two.includes(value))[0] };
 const errorIn = (line) => duplicateIn(line.substring(0, line.length / 2).split(''), line.substring(line.length / 2).split(''));
-const errors = (file) => [errorIn('vJrwpWtwJgWrhcsFMMfFFhFp'), errorIn('jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL'), 'P', 'v', 't', 's'];
+const errors = (file) => lines(file).map(line => errorIn(line));
 const priorities = (file) => errors(file).map(error => priorityOf(error));
 
 const solve1 = (file) => {
