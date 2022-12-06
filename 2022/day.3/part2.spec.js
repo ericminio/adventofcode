cfb8bb7c3e76381b98a8ce0d5a17e84169b9f971
ericminio <emignot@change.org>
2022-12-06 11:19:03 -0800

    ttt

--- 2022/day.3/part2.spec.js
+++ 2022/day.3/part2.spec.js
@@ -6,5 +6,9 @@ describe('2022.3.2', () => {
     it('has an example', () => {
         expect(solve2(`${__dirname}/data/example.txt`)).to.equal(70);
     });
+
+    it('is solved again', () => {
+        expect(solve2(`${__dirname}/data/example.txt`)).to.equal(70);
+    });
 });
 
