820d4137a3c4a091dceba7124a608b13455d6f1f
ericminio <eric.mignot@gmail.com>
2022-12-05 22:25:09 -0800

    ttt

--- 2022/support/index.js
+++ 2022/support/index.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 const input = (file) => fs.readFileSync(file).toString();
-
+const lines = (file) => input(file).split(/\n/);
 const groups = (file) => input(file).split(/\n\n/).map(items => items.split('\n'));
+
 const numberOrZero = (item => Number.isNaN(parseInt(item)) ? 0 : parseInt(item));
 const groupsOfNumbers = (file) => groups(file).map(items => items.map(item => numberOrZero(item)));
 const total = (array) => array.reduce((acc, current) => acc += current, 0);
 const orderDescending = (array) => { array.sort((a, b) => b - a); }
 
-const lines = (file) => input(file).split(/\n/);
 const rounds = (file) => lines(file).map(line => ({ line:line, opponent:line.charAt(0), me:line.charAt(2) }));
 const opponentScore = { 'A':1, 'B':2, 'C': 3 };
 const myScore = { 'X':1, 'Y':2, 'Z': 3 };
