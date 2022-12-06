056b1da6b56b7e66fed317d2fc95a02abb7bbac8
ericminio <emignot@change.org>
2022-12-06 09:09:01 -0800

    ttt

--- 2022/day.3/solve.js
+++ 2022/day.3/solve.js
@@ -1,7 +1,7 @@
 const { total } = require('../support');
 
 const priorityOf = (letter) => letter.charCodeAt(0) - 96;
-const priorities = (file) => [priorityOf('p'), 38, 42, 22, 20, 19];
+const priorities = (file) => [priorityOf('p'), 38, 42, priorityOf('v'), 20, 19];
 
 const solve1 = (file) => {
     return total(priorities(file));
