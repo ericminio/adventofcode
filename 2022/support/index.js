bc34bb99c6b5f75bf60549ca16bc329aaf9aebed
ericminio <eric.mignot@gmail.com>
2022-12-05 22:11:44 -0800

    ttt

--- 2022/support/index.js
+++ 2022/support/index.js
@@ -24,8 +24,8 @@ const outcome1 = (file) => shapeScore(file).map(round => ({
 const scores1 = (file) => outcome1(file).map(round => round.myShapeScore + round.outcome1);
 const outcome2 = (file) => rounds(file).map(round => ({
     ...round,
-    me:null,
     outcome2: outcomeCall[round.me],
+    me:undefined,
 }));
 
 module.exports = {
