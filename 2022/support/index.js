0a8ab20b93241dbb8da4905eae9ba62fc6232fc9
ericminio <eric.mignot@gmail.com>
2022-12-05 21:29:37 -0800

    ttt

--- 2022/support/index.js
+++ 2022/support/index.js
@@ -11,7 +11,11 @@ const lines = (file) => input(file).split(/\n/);
 const rounds = (file) => lines(file).map(line => ({ opponent:line.charAt(0), me:line.charAt(2) }));
 const shapeScore = { 'X':1, 'Y':2, 'Z': 3 };
 const opponentScore = { 'A':1, 'B':2, 'C': 3 };
-const selectionScore = (file) => rounds(file).map(round => ({...round, shapeScore:shapeScore[round.me] }));
+const selectionScore = (file) => rounds(file).map(round => ({
+    ...round, 
+    shapeScore:shapeScore[round.me],
+    opponentScore: opponentScore[round.ooponent],
+}));
 
 module.exports = {
     groupsOfNumbers,
