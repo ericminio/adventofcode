00e6355cef7f085c034a016fae0b7297a92b6481
ericminio <eric.mignot@gmail.com>
2022-12-05 21:12:11 -0800

    ttt

--- 2022/support/index.js
+++ 2022/support/index.js
@@ -1,16 +1,18 @@
 const fs = require('fs');
-
 const input = (file) => fs.readFileSync(file).toString();
+
 const groups = (file) => input(file).split(/\n\n/).map(items => items.split('\n'));
 const numberOrZero = (item => Number.isNaN(parseInt(item)) ? 0 : parseInt(item));
 const groupsOfNumbers = (file) => groups(file).map(items => items.map(item => numberOrZero(item)));
 const total = (array) => array.reduce((acc, current) => acc += current, 0);
 const orderDescending = (array) => { array.sort((a, b) => b - a); }
 
+const lines = (file) => input(file.split(/\n/));
 
 module.exports = {
-    input,
     groupsOfNumbers,
     total,
     orderDescending,
+
+    lines,
 };
