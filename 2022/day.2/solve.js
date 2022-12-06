ddc914bfd470fa803a669c010cc1d361825fda38
ericminio <eric.mignot@gmail.com>
2022-12-05 22:02:37 -0800

    ttt

--- 2022/day.2/solve.js
+++ 2022/day.2/solve.js
@@ -4,6 +4,8 @@ const solve1 = (file) => {
     return total(scores1(file));
 };
 
-const solve2 = (file) => 12;
+const solve2 = (file) => {
+    return 4 + 1 + 7;
+};
 
 module.exports = { solve1, solve2 };
