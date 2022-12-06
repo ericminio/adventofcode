2389bcb06da2c9553ee00c05852b462585433b25
ericminio <emignot@change.org>
2022-12-06 08:57:09 -0800

    ttt

---
+++ 2022/day.3/solve.js
@@ -0,0 +1,11 @@
+const { scores1, total, scores2 } = require('../support');
+
+const solve1 = (file) => {
+    return total(scores1(file));
+};
+
+const solve2 = (file) => {
+    return total(scores2(file));
+};
+
+module.exports = { solve1, solve2 };
