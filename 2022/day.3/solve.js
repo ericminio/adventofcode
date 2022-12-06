d086135c78dc2ae1dbaf41ee1145a9392e7a0e14
ericminio <emignot@change.org>
2022-12-06 09:01:44 -0800

    ttt

--- 2022/day.3/solve.js
+++ 2022/day.3/solve.js
@@ -1,7 +1,7 @@
 const { total } = require('../support');
-
+const priorities = (file) => [16, 38, 42, 22, 20, 19];
 const solve1 = (file) => {
-    return total([16, 38, 42, 22, 20, 19]);
+    return total(priorities(file));
 };
 
 module.exports = { solve1 };
