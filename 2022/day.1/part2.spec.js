d7e9224c65a73054094ee2fd1a2d63caee5c538e
ericminio <emignot@change.org>
2022-12-05 20:09:59 -0800

    ttt

--- 2022/day.1/part2.spec.js
+++ 2022/day.1/part2.spec.js
@@ -4,11 +4,11 @@ const { solve2 } = require('./solve');
 describe('part 2', () => {
 
     it('has an example', () => {
-        expect(solve2(`${__dirname}/example.txt`)).to.equal(45000);
+        expect(solve2(`${__dirname}/data/example.txt`)).to.equal(45000);
     });
 
     it('is solved', () => {
-        expect(solve2(`${__dirname}/input.txt`)).to.equal(213089);
+        expect(solve2(`${__dirname}/data/input.txt`)).to.equal(213089);
     });
 });
 
