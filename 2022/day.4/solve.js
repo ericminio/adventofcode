573b2e6963de146ae61b95e776d67c39e7b15111
ericminio <eric.mignot@gmail.com>
2022-12-06 20:48:56 -0800

    ttt

--- 2022/day.4/solve.js
+++ 2022/day.4/solve.js
@@ -5,12 +5,7 @@ const parse = (line) => pattern.exec(line).splice(1).map(item => parseInt(item))
 const build = (data) => ({ one: { start: data[0], end: data[1] }, two: { start: data[2], end: data[3] } });
 const isContained = (candidate, reference) => candidate.start >= reference.start && candidate.end <= reference.end;
 const contained = (pair) => isContained(pair.two, pair.one) || isContained(pair.one, pair.two);
-const fullyContained = file => [
-    0, 0, 0,
-    contained({ one: { start: 2, end: 8 }, two: { start: 3, end: 7 } }),
-    contained(build(parse('6-6,4-6'))),
-    contained(build(parse('2-6,4-8')))
-];
+const fullyContained = file => lines(file).map(line => contained(build(parse(line))));
 
 const solve1 = (file) => {
     return total(fullyContained(file));
