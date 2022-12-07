06b66c66674057feb3e4dce877d981cf5b9b0db4
ericminio <eric.mignot@gmail.com>
2022-12-06 21:39:33 -0800

    ttt

--- 2022/day.4/solve.js
+++ 2022/day.4/solve.js
@@ -2,11 +2,11 @@ const { total, lines, extractor, isInside, isOverlapping } = require('../support
 
 const pattern = /^(.*)-(.*),(.*)-(.*)$/;
 const parse = data => data.map(value => parseInt(value));
-const buildAssignments = (numbers) => ({
+const builder = (numbers) => ({
     one: { start: numbers[0], end: numbers[1] },
     two: { start: numbers[2], end: numbers[3] },
 });
-const assignments = (file) => lines(file).map(extractor(pattern)).map(parse).map(buildAssignments);
+const assignments = (file) => lines(file).map(extractor(pattern)).map(parse).map(builder);
 
 const areContaining = pair => isInside(pair.two, pair.one) || isInside(pair.one, pair.two);
 const containing = file => assignments(file).map(areContaining);
