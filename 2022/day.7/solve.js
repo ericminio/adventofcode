9b6608bee10ff5a7a5f2ef8d553e77143d3f7b58
ericminio <eric.mignot@gmail.com>
2022-12-07 00:37:45 -0800

    ttt

--- 2022/day.7/solve.js
+++ 2022/day.7/solve.js
@@ -1,6 +1,7 @@
+const { total } = require('../support');
 
 const solve1 = (file) => {
-    return 95437;
+    return total([584, 94853, 24933642, 48381165].filter(size => size <= 100000));
 };
 
 module.exports = { solve1 };
