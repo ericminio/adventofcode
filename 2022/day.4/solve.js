a552e108b2dec8897b02bec0e6ca956dbc6c86ba
ericminio <eric.mignot@gmail.com>
2022-12-06 20:53:37 -0800

    ttt

--- 2022/day.4/solve.js
+++ 2022/day.4/solve.js
@@ -1,7 +1,10 @@
 const { total, lines, parseForNumbers } = require('../support');
 
 const pattern = /^(.*)-(.*),(.*)-(.*)$/;
-const buildAssignments = (data) => ({ one: { start: data[0], end: data[1] }, two: { start: data[2], end: data[3] } });
+const buildAssignments = (data) => ({
+    one: { start: data[0], end: data[1] },
+    two: { start: data[2], end: data[3] }
+});
 const isContained = (candidate, reference) => candidate.start >= reference.start && candidate.end <= reference.end;
 const contained = (pair) => isContained(pair.two, pair.one) || isContained(pair.one, pair.two);
 const fullyContained = file => lines(file).map(line => contained(buildAssignments(parseForNumbers(line, pattern))));
