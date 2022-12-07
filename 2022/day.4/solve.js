cafecbcf3018007e419388f02cc9c2eb41f01f37
ericminio <eric.mignot@gmail.com>
2022-12-06 20:34:10 -0800

    ttt

--- 2022/day.4/solve.js
+++ 2022/day.4/solve.js
@@ -2,7 +2,12 @@ const { total, lines } = require('../support');
 
 const isContained = (candidate, reference) => candidate.start >= reference.start && candidate.end <= reference.end;
 const contained = (pair) => isContained(pair.two, pair.one);
-const fullyContained = file => [0, 0, 0, 1, contained({ one: { start: 2, end: 8 }, two: { start: 3, end: 7 } }), 0];
+const fullyContained = file => [
+    0, 0, 0,
+    contained({ one: { start: 2, end: 8 }, two: { start: 3, end: 7 } }),
+    1,
+    0
+];
 
 const solve1 = (file) => {
     return total(fullyContained(file));
