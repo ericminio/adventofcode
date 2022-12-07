0d2b14721364363ae800972a50d0dbe1aabbfd3e
ericminio <emignot@change.org>
2022-12-06 16:49:31 -0800

    ttt

--- 2022/day.4/solve.js
+++ 2022/day.4/solve.js
@@ -1,7 +1,10 @@
-const { total } = require('../support');
+const { total, lines } = require('../support');
+
+const isFullyContained = (line, index) => [3, 4].includes(index) ? 1 : 0;
+const fullyContained = file => lines(file).map(isFullyContained);
 
 const solve1 = (file) => {
-    return total([0, 0, 0, 1, 1, 0]);
+    return total(fullyContained(file));
 }
 
 module.exports = { solve1 };
