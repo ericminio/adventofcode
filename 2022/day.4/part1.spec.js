f128f2a02f2c26e5c2fcb95e6ba697506d494fd8
ericminio <eric.mignot@gmail.com>
2022-12-06 20:56:38 -0800

    ttt

--- 2022/day.4/part1.spec.js
+++ 2022/day.4/part1.spec.js
@@ -6,5 +6,8 @@ describe('2022.4.1', () => {
     it('has an example', () => {
         expect(solve1(`${__dirname}/data/example.txt`)).to.equal(2);
     });
+    it('is solved', () => {
+        expect(solve1(`${__dirname}/data/example.txt`)).to.equal(2);
+    });
 });
 
