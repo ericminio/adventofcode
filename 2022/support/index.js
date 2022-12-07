e7b58dfdc6c02fe1bbdc46e1b3e1b2f28e0e40be
ericminio <eric.mignot@gmail.com>
2022-12-06 21:26:35 -0800

    ttt

--- 2022/support/index.js
+++ 2022/support/index.js
@@ -20,6 +20,7 @@ const groupsOfNumbers = (file) => groups(file).map(items => items.map(item => nu
 const total = (array) => array.reduce((acc, current) => acc += current, 0);
 const orderDescending = (array) => { array.sort((a, b) => b - a); }
 
+const parser = (pattern) => (line) => pattern.exec(line).splice(1);
 const parse = (line, pattern) => pattern.exec(line).splice(1);
 const parseForNumbers = (line, pattern) => parse(line, pattern).map(item => parseInt(item));
 
@@ -32,6 +33,7 @@ module.exports = {
     groupsOfNumbers,
     total,
     orderDescending,
+    parser,
     parse,
     parseForNumbers,
     isInside,
