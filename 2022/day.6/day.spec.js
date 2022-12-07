c1359338f10afacbe8fa0b1e4f6d71a7c54d1a55
ericminio <eric.mignot@gmail.com>
2022-12-07 00:21:54 -0800

    ttt

--- 2022/day.6/day.spec.js
+++ 2022/day.6/day.spec.js
@@ -1,5 +1,5 @@
 const { expect } = require('chai');
-const { solve1 } = require('./solve');
+const { solve1, solve2 } = require('./solve');
 
 describe('2022.6.1', () => {
 
@@ -8,6 +8,13 @@ describe('2022.6.1', () => {
     });
     it('is solved', () => {
         expect(solve1(`${__dirname}/data/input.txt`)).to.equal(1848);
+    });
+});
+
+describe('2022.6.2', () => {
+
+    it('has an example', () => {
+        expect(solve2(`${__dirname}/data/example.txt`)).to.equal(19);
     });
 });
 
