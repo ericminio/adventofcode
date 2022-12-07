14ada77a621e41d159e6ad3e28bdb29fe97a4e90
ericminio <eric.mignot@gmail.com>
2022-12-07 00:49:15 -0800

    ttt

--- 2022/day.7/solve.js
+++ 2022/day.7/solve.js
@@ -1,11 +1,13 @@
 const { total } = require('../support');
 
-const inspect = (file) => [
-    { name: '/a/e', size: 584 },
-    { name: '/a', size: 94853 },
-    { name: '/d', size: 24933642 },
-    { name: '/', size: 48381165 },
-];
+const inspect = (file) => {
+    return [
+        { name: '/a/e', size: 584 },
+        { name: '/a', size: 94853 },
+        { name: '/d', size: 24933642 },
+        { name: '/', size: 48381165 },
+    ]
+};
 
 const solve1 = (file) => {
     return total(
