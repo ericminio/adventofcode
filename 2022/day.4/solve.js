a9bfadb9898d10bd6046fb64d561799b834ec2df
ericminio <eric.mignot@gmail.com>
2022-12-06 21:38:03 -0800

    ttt

--- 2022/day.4/solve.js
+++ 2022/day.4/solve.js
@@ -6,10 +6,7 @@ const buildAssignments = (numbers) => ({
     one: { start: numbers[0], end: numbers[1] },
     two: { start: numbers[2], end: numbers[3] },
 });
-const assignments = (file) => lines(file)
-    .map(extractor(pattern))
-    .map(parse)
-    .map(buildAssignments);
+const assignments = (file) => lines(file).map(extractor(pattern)).map(parse).map(buildAssignments);
 const areContaining = pair => isInside(pair.two, pair.one) || isInside(pair.one, pair.two);
 const containing = file => assignments(file).map(areContaining);
 
