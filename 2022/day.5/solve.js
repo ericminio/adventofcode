1975fb2deec5b4439f64d091acb95a7d8157cc49
ericminio <eric.mignot@gmail.com>
2022-12-06 22:36:51 -0800

    ttt

--- 2022/day.5/solve.js
+++ 2022/day.5/solve.js
@@ -1,7 +1,10 @@
 const first = (stack) => stack[0];
+const rearrange = (stacks, move) => [['C'], ['M'], ['Z', 'N', 'D', 'P']];
 
 const solve1 = (file) => {
-    return [['C'], ['M'], ['Z', 'N', 'D', 'P']].map(first).join('');
+    let current = [['M', 'C'], [], ['Z', 'N', 'D', 'P']];
+    let next = rearrange(current, { count: 1, from: 0, to: 1 });
+    return next.map(first).join('');
 };
 
 module.exports = { solve1 };
