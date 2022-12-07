d6fac4eb1abeea217137e35be177c08ccd831302
ericminio <emignot@change.org>
2022-12-07 11:02:44 -0800

    ttt

--- 2022/day.7/solve.js
+++ 2022/day.7/solve.js
@@ -14,7 +14,7 @@ const inspect = (file) => {
         }
         if (isChangeDirDown(line)) {
             let name = line.substring(5).trim();
-            current = name === separator ? name : current + `/${name}`;
+            current = name === separator ? separator : current + `/${name}`;
             folders.push({ name: current, size: 0 });
         }
         if (isFileInfo(line)) {
