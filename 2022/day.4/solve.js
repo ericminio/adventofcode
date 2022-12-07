65e563557ec449be61aba3f09b7bb73fffc2fdcc
ericminio <eric.mignot@gmail.com>
2022-12-06 20:56:03 -0800

    ttt

--- 2022/day.4/solve.js
+++ 2022/day.4/solve.js
@@ -7,10 +7,10 @@ const buildAssignments = (data) => ({
 });
 const isContained = (candidate, reference) => candidate.start >= reference.start && candidate.end <= reference.end;
 const shouldReconsider = (pair) => isContained(pair.two, pair.one) || isContained(pair.one, pair.two);
-const fullyContained = file => lines(file).map(line => shouldReconsider(buildAssignments(parseForNumbers(line, pattern))));
+const toReconsider = file => lines(file).map(line => shouldReconsider(buildAssignments(parseForNumbers(line, pattern))));
 
 const solve1 = (file) => {
-    return total(fullyContained(file));
+    return total(toReconsider(file));
 }
 
 module.exports = { solve1 };
