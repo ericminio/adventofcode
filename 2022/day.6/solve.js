08a0ca64b95832718a9c3918d43796804183bc68
ericminio <eric.mignot@gmail.com>
2022-12-07 00:17:39 -0800

    ttt

--- 2022/day.6/solve.js
+++ 2022/day.6/solve.js
@@ -2,8 +2,17 @@ const { input } = require('../support');
 
 const solve1 = (file) => {
     const data = input(file);
-    for (var i = 4; i < data.length; i++) {
-        let marker = {}
+    for (var i = 3; i < data.length; i++) {
+        let marker = {};
+        for (j = i; j >= i - 3; j--) {
+            let digit = data.charAt(j);
+            if (marker[digit] === undefined) {
+                marker[digit] = 1;
+            }
+            else {
+                marker[digit]++;
+            }
+        }
     }
     return 7;
 }
