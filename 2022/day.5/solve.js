31b3dfae801a892f8b5cc0df8e334a7f8b4aaae4
ericminio <eric.mignot@gmail.com>
2022-12-06 22:24:33 -0800

    ttt

--- 2022/day.5/solve.js
+++ 2022/day.5/solve.js
@@ -1,20 +1,5 @@
-const { total, lines, extractor, isInside, isOverlapping } = require('../support');
-
-const pattern = /^(.*)-(.*),(.*)-(.*)$/;
-const builder = (data) => ({
-    one: { start: parseInt(data[0]), end: parseInt(data[1]) },
-    two: { start: parseInt(data[2]), end: parseInt(data[3]) },
-});
-const pairs = (file) => lines(file).map(extractor(pattern)).map(builder);
-
-const isContainingPair = pair => isInside(pair.two, pair.one) || isInside(pair.one, pair.two);
-const isOverlappingPair = pair => isOverlapping(pair.one, pair.two);
-
 const solve1 = (file) => {
-    return total(pairs(file).map(isContainingPair));
-};
-const solve2 = (file) => {
-    return total(pairs(file).map(isOverlappingPair));
+    return 'CMZ';
 };
 
-module.exports = { solve1, solve2 };
+module.exports = { solve1 };
