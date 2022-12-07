5511eb90527fb515a6b018f756b29eead8f8ce8b
ericminio <eric.mignot@gmail.com>
2022-12-06 20:33:13 -0800

    ttt

--- 2022/day.4/solve.js
+++ 2022/day.4/solve.js
@@ -1,6 +1,6 @@
 const { total, lines } = require('../support');
 
-const isContained = (candidate, reference) => 1;
+const isContained = (candidate, reference) => candidate.start >= reference.start && candidate.end <= reference.end;
 const contained = (pair) => isContained(pair.two, pair.one);
 const fullyContained = file => [0, 0, 0, 1, contained({ one: { start: 2, end: 8 }, two: { start: 3, end: 7 } }), 0];
 
