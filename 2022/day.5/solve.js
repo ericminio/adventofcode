e0d1db498064d2a959b1cb03c30449e905a84bf8
ericminio <eric.mignot@gmail.com>
2022-12-06 22:57:58 -0800

    ttt

--- 2022/day.5/solve.js
+++ 2022/day.5/solve.js
@@ -18,14 +18,7 @@ const solve1 = (file) => {
 
 const parseMoves = (file) => {
     const pattern = /^move\s(.*)\sfrom\s(.*)\sto\s(.*)$/;
-    const moves = groups(file)[1].map(extractor(pattern)).map(builder)
-    console.log(moves)
-    return [
-        { count: 1, from: 1, to: 0 },
-        { count: 3, from: 0, to: 2 },
-        { count: 2, from: 1, to: 0 },
-        { count: 1, from: 0, to: 1 },
-    ];
+    return groups(file)[1].map(extractor(pattern)).map(builder);
 };
 const parseStacks = (file) => {
     return [['Z', 'N'], ['M', 'C', 'D'], ['P']];
