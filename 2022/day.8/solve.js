f2e17bde19d17859a64697c7e3d3d44f65bcfa54
ericminio <eric.mignot@gmail.com>
2022-12-07 21:32:12 -0800

    ttt

--- 2022/day.8/solve.js
+++ 2022/day.8/solve.js
@@ -7,7 +7,7 @@ const isVisible = (tree, forest) => isVisibleFromTop(tree, forest);
 const solve1 = (file) => {
     const forest = lines(file);
     return perimeter(forest) +
-        isVisible({ x: 1, y: 1 }, forest) + 1 + 0 +
+        isVisible({ x: 1, y: 1, height: 5 }, forest) + 1 + 0 +
         1 + 0 + 1 +
         0 + 1 + 0
         ;
