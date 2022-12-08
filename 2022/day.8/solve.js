bbdf6abb0c7bc08db3e875446fe95a1ef2fd8bc0
ericminio <eric.mignot@gmail.com>
2022-12-07 21:22:46 -0800

    ttt

--- 2022/day.8/solve.js
+++ 2022/day.8/solve.js
@@ -4,7 +4,7 @@ const perimeter = (forest) => 2 * forest.length + 2 * (forest[0].length - 2);
 
 const solve1 = (file) => {
     const forest = lines(file);
-    return perimeter(forest) + 5;
+    return perimeter(forest) + 2 + 3;
 };
 
 module.exports = { solve1 };
