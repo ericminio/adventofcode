22440dd41b11bcb9e6be276bb83a4d125c215e4b
ericminio <eric.mignot@gmail.com>
2022-12-07 21:46:48 -0800

    ttt

--- 2022/day.8/solve.js
+++ 2022/day.8/solve.js
@@ -10,14 +10,23 @@ const isVisibleFromTop = (tree, forest) => {
         }
     }
     return true;
-}
-const isVisible = (tree, forest) => isVisibleFromTop(tree, forest);
+};
+const isVisibleFromRight = (tree, forest) => {
+    for (var j = tree.y + 1; j < forest.length; j++) {
+        let candidate = treeAt(tree.x, j, forest);
+        if (tree.height <= candidate.height) {
+            return false;
+        }
+    }
+    return true;
+};
+const isVisible = (tree, forest) => isVisibleFromTop(tree, forest) || isVisibleFromRight(tree, forest);
 
 const solve1 = (file) => {
     const forest = lines(file);
     return perimeter(forest) +
         isVisible(treeAt(1, 1, forest), forest) + 1 + 0 +
-        1 + 0 + 1 +
+        isVisible(treeAt(2, 1, forest), forest) + 0 + 1 +
         0 + 1 + 0
         ;
 };
