02594c5823f6fdb556cd8ae1a2e9aa4f5faa0d9d
ericminio <eric.mignot@gmail.com>
2022-12-07 21:30:10 -0800

    ttt

--- 2022/day.8/solve.js
+++ 2022/day.8/solve.js
@@ -1,11 +1,12 @@
 const { lines } = require('../support');
 
 const perimeter = (forest) => 2 * forest.length + 2 * (forest[0].length - 2);
+const isVisible = (tree, forest) => true;
 
 const solve1 = (file) => {
     const forest = lines(file);
     return perimeter(forest) +
-        1 + 1 + 0 +
+        isVisible({ x: 1, y: 1 }, forest) + 1 + 0 +
         1 + 0 + 1 +
         0 + 1 + 0
         ;
