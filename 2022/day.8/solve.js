4be34866ba1af12c012b75417e0a065138edbbc1
ericminio <eric.mignot@gmail.com>
2022-12-07 21:21:11 -0800

    ttt

--- 2022/day.8/solve.js
+++ 2022/day.8/solve.js
@@ -1,6 +1,6 @@
 const { lines } = require('../support');
 
-const perimeter = (forect) => 16;
+const perimeter = (forest) => 2 * forest.length + 2 * (forest[0].length - 2);
 
 const solve1 = (file) => {
     const forest = lines(file);
