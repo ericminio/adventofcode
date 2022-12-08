65543195d8a3bb1cd14b4a337df638e8a96f9124
ericminio <eric.mignot@gmail.com>
2022-12-07 21:06:59 -0800

    ttt

---
+++ 2022/day.template copy/day.spec.js
@@ -0,0 +1,23 @@
+const { expect } = require('chai');
+const { solve1, solve2 } = require('./solve');
+
+describe('2022.?', () => {
+
+    describe('part 1', () => {
+
+        it('has an example', () => {
+            expect(solve1(`${__dirname}/data/example.txt`)).to.equal(42);
+        });
+
+    });
+
+    describe('part 2', () => {
+
+        it('has an example', () => {
+            expect(solve2(`${__dirname}/data/example.txt`)).to.equal(15);
+        });
+
+    });
+});
+
+
