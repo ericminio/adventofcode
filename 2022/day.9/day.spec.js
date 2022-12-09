455ca795b25c3d36559927c166b24bb9b522379d
ericminio <emignot@change.org>
2022-12-08 21:41:42 -0800

    ttt

--- 2022/day.9/day.spec.js
+++ 2022/day.9/day.spec.js
@@ -1,5 +1,7 @@
 const { expect } = require('chai');
 const { solve1, solve2 } = require('./solve');
+const { parse } = require('./solve');
+const { lines } = require('../support');
 
 describe.only('2022.9', () => {
 
