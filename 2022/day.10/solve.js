b948b53e263e4c160d214b809a7057c886cbc629
ericminio <emignot@change.org>
2022-12-12 09:15:55 -0800

    ttt

--- 2022/day.10/solve.js
+++ 2022/day.10/solve.js
@@ -1,8 +1,12 @@
 const { sum } = require('../support');
 
+const valueDuringCycle = (value) => {
+    return 420;
+}
+
 const solve1 = (file) => {
     const points = [20, 60, 100, 140, 180, 220];
-    return sum([420, 1140, 1800, 2940, 2880, 3960]);
+    return sum([valueDuringCycle(points[0]), 1140, 1800, 2940, 2880, 3960]);
 };
 
 const solve2 = (file) => {
