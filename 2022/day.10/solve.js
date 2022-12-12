6b746e6456a35655517043be8cf1a95ea879894c
ericminio <emignot@change.org>
2022-12-12 09:11:39 -0800

    ttt

--- 2022/day.10/solve.js
+++ 2022/day.10/solve.js
@@ -1,5 +1,7 @@
+const { sum } = require('../support');
+
 const solve1 = (file) => {
-    return 420 + 1140 + 1800 + 2940 + 2880 + 3960;
+    return sum([420, 1140, 1800, 2940, 2880, 3960]);
 };
 
 const solve2 = (file) => {
