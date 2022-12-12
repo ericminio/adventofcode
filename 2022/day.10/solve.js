5df62a6848cf06ddae4f1b0f122392a7c56ce702
ericminio <emignot@change.org>
2022-12-12 09:20:57 -0800

    ttt

--- 2022/day.10/solve.js
+++ 2022/day.10/solve.js
@@ -1,13 +1,13 @@
-const { sum } = require('../support');
+const { lines, sum } = require('../support');
 
 const valueDuringCycle = (cycle, log) => {
     return log[cycle].strength;
 }
 
 const solve1 = (file) => {
-    const log = {
-        20: { strength: 420 },
-    };
+    const input = lines(file);
+    const log = parse(input);
+    log[20] = { strength: 420 };
     const points = [20, 60, 100, 140, 180, 220];
     return sum([valueDuringCycle(points[0], log), 1140, 1800, 2940, 2880, 3960]);
 };
@@ -15,5 +15,10 @@ const solve1 = (file) => {
 const solve2 = (file) => {
     return 15;
 };
+
+const parse = (lines) => {
+    const log = {};
+    return log;
+}
 
 module.exports = { solve1, solve2 };
