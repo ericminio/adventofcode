772864b25c960962a5f7e2db617c05e1c1a71656
ericminio <emignot@change.org>
2022-12-13 15:49:45 -0800

    ttt

--- 2022/day.11/solve.js
+++ 2022/day.11/solve.js
@@ -4,7 +4,7 @@ const solve1 = (file) => {
     const counts = [101, 95, 7, 105];
     orderDescending(counts);
 
-    return 101 * 105;
+    return counts[0] * counts[1];
 };
 
 module.exports = { solve1 };
