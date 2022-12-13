939729b549cf9aa7d9db8e8bff9706e11828f055
ericminio <emignot@change.org>
2022-12-13 12:11:07 -0800

    ttt

--- 2022/day.10/solve.js
+++ 2022/day.10/solve.js
@@ -5,7 +5,25 @@ const logCycle = (log, line, cycle, register) => {
 };
 const valueDuringCycle = (cycle, log) => {
     return log[cycle].strength;
-}
+};
+
+const run = (lines) => {
+    const log = {};
+    let cycle = 1;
+    let register = 1;
+    for (var i = 0; i < lines.length; i++) {
+        const line = lines[i];
+        logCycle(log, line, cycle, register);
+        cycle++;
+        if (line !== 'noop') {
+            logCycle(log, line, cycle, register);
+            register += parseInt(line.substring(5));
+            cycle++;
+        }
+    }
+
+    return log;
+};
 
 const solve1 = (file) => {
     const input = lines(file);
@@ -35,24 +53,6 @@ const solve2 = (file) => {
     }
 
     return rows;
-};
-
-const run = (lines) => {
-    const log = {};
-    let cycle = 1;
-    let register = 1;
-    for (var i = 0; i < lines.length; i++) {
-        const line = lines[i];
-        logCycle(log, line, cycle, register);
-        cycle++;
-        if (line !== 'noop') {
-            logCycle(log, line, cycle, register);
-            register += parseInt(line.substring(5));
-            cycle++;
-        }
-    }
-
-    return log;
 };
 
 module.exports = { solve1, solve2 };
