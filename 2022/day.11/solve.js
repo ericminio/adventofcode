4617a70e0d194baf5cf9cd6a3124063a24f7c0e0
ericminio <emignot@change.org>
2022-12-13 16:00:02 -0800

    ttt

--- 2022/day.11/solve.js
+++ 2022/day.11/solve.js
@@ -1,7 +1,21 @@
 const { orderDescending } = require('../support');
 
 const solve1 = (file) => {
-    const counts = [101, 95, 7, 105];
+    const monkeys = [
+        {
+            count: 101,
+        },
+        {
+            count: 95,
+        },
+        {
+            count: 7,
+        },
+        {
+            count: 105,
+        },
+    ]
+    const counts = monkeys.map(monkey => monkey.count);
     orderDescending(counts);
 
     return counts[0] * counts[1];
