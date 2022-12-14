12e88f65884a1be9c8f068cf3641c0c05110dc75
ericminio <emignot@change.org>
2022-12-14 13:54:56 -0800

    ttt

--- 2022/day.template/day.spec.js
+++ 2022/day.template/day.spec.js
@@ -1,7 +1,7 @@
 const { expect } = require('chai');
 const { solve1, solve2 } = require('./solve');
 
-describe('2022.?', () => {
+describe.only('2022.12', () => {
 
     describe('part 1', () => {
 
