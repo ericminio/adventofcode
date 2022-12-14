7129935966ae1c4a66b01f526ec3ff9b4ccb0f2b
ericminio <emignot@change.org>
2022-12-13 21:24:16 -0800

    ttt

--- 2022/day.1/solve.js
+++ 2022/day.1/solve.js
@@ -1,7 +1,10 @@
 const { descending, groupsOfNumbers, orderDescending, total } = require('../support');
 
 const solve1 = (file) => {
-    return groupsOfNumbers(file).map(group => total(group)).sort(descending)[0];
+    return groupsOfNumbers(file)
+        .map(group => total(group))
+        .sort(descending)
+    [0];
 };
 
 const solve2 = (file) => {
