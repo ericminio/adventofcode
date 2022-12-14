a785f6e0c7b350a5b5d650bf752d71a74d7b4866
ericminio <emignot@change.org>
2022-12-13 16:50:27 -0800

    ttt

--- 2022/day.11/solve.js
+++ 2022/day.11/solve.js
@@ -27,7 +27,7 @@ const solve1 = (file) => {
 const solve2 = (file) => {
     const monkeys = parse(file);
 
-    let count = 20;
+    let count = 1000;
     while (count > 0) {
         monkeys.forEach(monkey => {
             monkey.items.forEach(value => {
