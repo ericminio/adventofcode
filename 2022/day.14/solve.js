4197002ecefa1c4f4b095f380faa586ad31aebd2
ericminio <emignot@change.org>
2022-12-13 22:09:26 -0800

    ttt

--- 2022/day.14/solve.js
+++ 2022/day.14/solve.js
@@ -1,5 +1,7 @@
+const ROCK = 1;
+const SAND = 2;
 const solve1 = (file) => {
-    let obstacles = {};
+    let obstacles = { '500x9': ROCK, '499x9': ROCK, '501x9': ROCK };
     let resting = [];
 
     let candidate = { x: 500, y: 8 };
@@ -11,6 +13,7 @@ const solve1 = (file) => {
 };
 
 const blocked = (candidate, obstacles) => {
+
     return true;
 };
 
