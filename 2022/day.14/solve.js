dbdbbaa858bb541de26bc019635870a028476fc9
ericminio <emignot@change.org>
2022-12-14 09:28:02 -0800

    ttt

--- 2022/day.14/solve.js
+++ 2022/day.14/solve.js
@@ -54,9 +54,9 @@ const parse = (file) => {
     let obstacles = {};
     let bottom = 9;
 
-    let input = lines(file);
-    let p = input.map(line => line.split('->').map(p => p.split(',')).map(p => p.map(v => parseInt(v))));
-    console.log(p);
+    let input = lines(file)
+        .map(line => line.split('->').map(p => p.split(',')).map(p => p.map(v => parseInt(v))));
+    console.log(input);
 
     let line = '503,4 -> 502,4 -> 502,9 -> 494,9';
     let points = line.split('->').map(p => p.split(',')).map(p => p.map(v => parseInt(v)));
