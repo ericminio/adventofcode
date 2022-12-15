1385baecc589555633f006db1688b962c2a3bd7d
ericminio <emignot@change.org>
2022-12-15 09:22:08 -0800

    ttt

--- 2022/day.15/solve.js
+++ 2022/day.15/solve.js
@@ -1,9 +1,5 @@
-const solve1 = (file) => {
-    return 42;
+const solve1 = (file, row) => {
+    return 26;
 };
 
-const solve2 = (file) => {
-    return 15;
-};
-
-module.exports = { solve1, solve2 };
+module.exports = { solve1 };
