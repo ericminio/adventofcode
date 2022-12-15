8d871dd64ffb0a97250ef1f80d64dfd9bed54417
ericminio <eric.mignot@gmail.com>
2022-12-14 22:39:54 -0800

    ttt

--- 2022/day.12/solve.js
+++ 2022/day.12/solve.js
@@ -4,4 +4,13 @@ const solve1 = (file) => {
     return 31;
 };
 
-module.exports = { solve1 };
+const load = (view) => {
+    let lines = view.trim().split('\n');
+    let size = { height: lines.length, width: lines[0].trim().length };
+    let map = mapAsHash({ height: size.height, width: size.width });
+
+
+    return map;
+}
+
+module.exports = { solve1, load };
