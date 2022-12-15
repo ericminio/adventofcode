e8cfe2c34de641f702160198c664a5b19d32e67f
ericminio <emignot@change.org>
2022-12-15 12:10:50 -0800

    ttt

--- 2022/day.15/solve.js
+++ 2022/day.15/solve.js
@@ -11,8 +11,9 @@ const solve2 = (file) => {
 
     let beacon = {};
     for (row = range.minimum.y; row <= range.maximum.y; row++) {
-        const points = coverage(row, sensors);
+        const points = Object.values(coverage(row, sensors));
         console.log(row, points);
+
     }
 
     beacon = { ...{ x: 14, y: 11 } };
