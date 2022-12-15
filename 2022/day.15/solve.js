969c1f3c14e588a2ec377a99abddef8c7987c98e
ericminio <emignot@change.org>
2022-12-15 09:42:30 -0800

    ttt

--- 2022/day.15/solve.js
+++ 2022/day.15/solve.js
@@ -1,4 +1,4 @@
-const { lines } = require('../support');
+const { extractor, lines } = require('../support');
 
 const solve1 = (file, row) => {
     const sensors = parse(file);
@@ -6,7 +6,18 @@ const solve1 = (file, row) => {
 };
 
 const parse = (file) => {
-    let input = lines(file);
+    let sensors = lines(file)
+        .map(extractor(/^Sensor at x=(.*), y=(.*): closest beacon is at x=(.*), y=(.*)$/))
+        .map(data => ({
+            x: parseInt(data[0]),
+            y: parseInt(data[1]),
+            beacon: {
+                x: parseInt(data[3]),
+                y: parseInt(data[4]),
+            },
+        }))
+        ;
+    console.log(sensors);
 };
 
 module.exports = { solve1 };
