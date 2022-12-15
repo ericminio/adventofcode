7488d400c69768452239d451a5ae75e665724938
ericminio <emignot@change.org>
2022-12-15 09:38:17 -0800

    ttt

--- 2022/day.15/solve.js
+++ 2022/day.15/solve.js
@@ -1,5 +1,12 @@
+const { lines } = require('../support');
+
 const solve1 = (file, row) => {
+    const sensors = parse(file);
     return 26;
+};
+
+const parse = (file) => {
+    let input = lines(file);
 };
 
 module.exports = { solve1 };
