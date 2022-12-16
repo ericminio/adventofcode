20d8c41b5157979d9dbd84fb28cc8af029bd3880
ericminio <emignot@change.org>
2022-12-15 20:37:19 -0800

    ttt

--- 2022/day.13/solve.js
+++ 2022/day.13/solve.js
@@ -1,10 +1,14 @@
 const { groupsOf } = require('../support');
 
 const solve1 = (file) => {
-    const groups = groupsOf(2, file);
+    const groups = parse(file);
     console.log(groups);
 
     return 13;
 };
+
+const parse = (file) => {
+    return groupsOf(2, file);
+}
 
 module.exports = { solve1 };
