90ab8aa38b021d611dbbc0a8c864ed7381cbffbc
ericminio <emignot@change.org>
2022-12-15 21:38:52 -0800

    ttt

--- 2022/day.13/solve.js
+++ 2022/day.13/solve.js
@@ -4,7 +4,7 @@ const solve1 = (file) => {
     const pairs = parse(file);
     console.log(pairs);
 
-    const statuses = [1, statusOf(pairs[1]), 0, 1, 0, 1, 0, 0]
+    const statuses = [1, 1, 0, 1, 0, 1, 0, 0]
 
     return statuses.map((value, index) => (index + 1) * value).reduce(add);
 };
@@ -27,8 +27,12 @@ const compareList = (left, right) => {
         index++;
         result = compareItem(left[index], right[index]);
     }
-    while (result !== RIGHT_ORDER && result !== NOT_RIGHT_ORDER && index < 15);
+    while (result !== RIGHT_ORDER && result !== NOT_RIGHT_ORDER && result !== BOTH_EMPTY && index < 15);
 
+    if (result === BOTH_EMPTY) {
+        result = DONT_KNOW_YET;
+    }
+    console.log('list comparaison ->', result);
     return result;
 };
 const compareItem = (left, right) => {
