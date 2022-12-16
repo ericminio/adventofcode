5186aef718602822e4e648863ced7991d7c4108d
ericminio <emignot@change.org>
2022-12-15 23:03:04 -0800

    ttt

--- 2022/day.16/day.spec.js
+++ 2022/day.16/day.spec.js
@@ -22,7 +22,10 @@ describe.only('2022.16', () => {
     describe('exploration', () => {
 
         it('is usefull', () => {
-
+            const lines = [
+                'Valve AA has flow rate=0; tunnels lead to valves DD, II, BB',
+                'Valve BB has flow rate=13; tunnels lead to valves CC, AA'
+            ];
 
         });
     });
