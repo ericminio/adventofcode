d59f17b991d4087d9d161a7ac0528aaad5bea20e
ericminio <emignot@change.org>
2022-12-15 22:44:56 -0800

    ttt

--- 2022/day.16/solve.js
+++ 2022/day.16/solve.js
@@ -1,9 +1,24 @@
+const { extractor, lines } = require('../support');
+
 const solve1 = (file) => {
+    const valves = parse(file);
+    console.log(valves);
     return 1651;
 };
 
 const solve2 = (file) => {
     return 15;
+};
+
+const parse = (file) => {
+    const pattern = /^Valve (.*) has flow rate=(.*); tunnels lead to valves (.*)$/;
+    return lines(file)
+        .map(line => pattern.exec(line));
+    // .map(data => ({
+    //     id: data[0],
+    //     rate: parseInt(data[1]),
+    //     next: data[2].split(','),
+    // }));
 };
 
 module.exports = { solve1, solve2 };
