430565c187d3a605bcee0a0145b4f3dbc22a48a6
ericminio <eric.mignot@gmail.com>
2022-12-17 10:36:02 -0800

    ttt

--- 2022/day.16/solve.js
+++ 2022/day.16/solve.js
@@ -6,6 +6,7 @@ const solve1 = (file) => {
     const table = distances(map);
 
     const valves = Object.values(map).filter(valve => valve.rate > 0);
+    console.log(valves);
     const ids = valves.map(valve => valve.id);
 
     return 1;
