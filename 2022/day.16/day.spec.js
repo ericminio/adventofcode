2ea5ac8330061f2f4546f88503fd7e719f1b2e41
ericminio <eric.mignot@gmail.com>
2022-12-16 21:37:50 -0800

    ttt

--- 2022/day.16/day.spec.js
+++ 2022/day.16/day.spec.js
@@ -64,7 +64,8 @@ describe.only('2022.16', () => {
             const map = parse(file);
             const valves = Object.values(map).filter(valve => valve.rate > 0);
             const ids = valves.map(valve => valve.id);
-            console.log(ids);
+            const candidates = permutations(ids);
+
         });
     });
 });
