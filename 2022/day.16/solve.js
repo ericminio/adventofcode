d35f94bb8a5c857a9275ec7e2001b8d13588d4e6
ericminio <eric.mignot@gmail.com>
2022-12-17 21:52:53 -0800

    ttt

--- 2022/day.16/solve.js
+++ 2022/day.16/solve.js
@@ -11,9 +11,18 @@ const solve1 = (file) => {
     console.log(candidates);
 
     let sorted = [];
+    let start;
 
-    let i = 0;
-    let start = candidates.shift();
+    start = candidates.shift();
+    sorted.push(start);
+    candidates.sort((a, b) => {
+        let aTHENb = (table[entry(start.id, a.id)] + 1) * a.rate + (table[entry(a.id, b.id)] + 1) * b.rate;
+        let bTHENa = (table[entry(start.id, b.id)] + 1) * b.rate + (table[entry(b.id, a.id)] + 1) * a.rate;
+        return aTHENb - bTHENa;
+    });
+    console.log(candidates);
+
+    start = candidates.shift();
     sorted.push(start);
     candidates.sort((a, b) => {
         let aTHENb = (table[entry(start.id, a.id)] + 1) * a.rate + (table[entry(a.id, b.id)] + 1) * b.rate;
