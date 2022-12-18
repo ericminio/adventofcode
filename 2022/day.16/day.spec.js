182c27d2f9c191bb71ade726b0125c2505a9c11d
ericminio <eric.mignot@gmail.com>
2022-12-18 11:59:40 -0800

    ttt

--- 2022/day.16/day.spec.js
+++ 2022/day.16/day.spec.js
@@ -53,7 +53,6 @@ describe.only('2022.16', () => {
         const table = distances(map);
 
         it.only('computes AJB as expected', () => {
-            console.log(table);
             let value = weight(map['AA'], map['JJ'], map['BB'], table, 26);
             console.log(value);
         });
