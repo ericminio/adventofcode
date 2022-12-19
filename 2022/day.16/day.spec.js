c3255f24dac0a6b753288b745a05ac6399d17847
ericminio <eric.mignot@gmail.com>
2022-12-18 19:42:08 -0800

    ttt

--- 2022/day.16/day.spec.js
+++ 2022/day.16/day.spec.js
@@ -1,12 +1,23 @@
 const { expect } = require('chai');
 const { gps } = require('../../lib/2d/gps.js');
-const { solve1, solve2, parse, timeSpent, weight, distances } = require('./solve');
+const { winner1, solve1, solve2, parse, timeSpent, weight, distances } = require('./solve');
 
 describe.only('2022.16', () => {
 
     describe('part 1', () => {
 
         describe('example', () => {
+
+            it('has a winning path', () => {
+                expect(winner1(`${__dirname}/data/example.txt`)).to.deep.equal([
+                    { id: 'DD', rate: 20, minutes: 2 },
+                    { id: 'BB', rate: 13, minutes: 5 },
+                    { id: 'JJ', rate: 21, minutes: 9 },
+                    { id: 'HH', rate: 22, minutes: 17 },
+                    { id: 'EE', rate: 3, minutes: 21 },
+                    { id: 'CC', rate: 2, minutes: 24 },
+                ]);
+            });
 
             it('is solved', () => {
                 expect(solve1(`${__dirname}/data/example.txt`)).to.equal(1651);
