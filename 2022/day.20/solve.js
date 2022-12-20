efe6cddf43aea26acad6f21e806a73d1c2b1386a
ericminio <eric.mignot@gmail.com>
2022-12-20 15:24:22 -0800

    ttt

--- 2022/day.20/solve.js
+++ 2022/day.20/solve.js
@@ -1,7 +1,9 @@
+const { add } = require('../support');
+
 const solve1 = (file) => {
     values = [1, 2, -3, 4, 0, 3, -2];
 
-    return 3;
+    return [4, -3, 2].reduce(add);
 };
 
 module.exports = { solve1 };
