9c6d855ff38247d1715a7507d3f9d5ce875fda56
ericminio <eric.mignot@gmail.com>
2022-12-20 18:11:16 -0800

    ttt

--- 2022/day.20/solve.js
+++ 2022/day.20/solve.js
@@ -4,17 +4,8 @@ const solve1 = (file) => {
     let incoming = lines(file).map(line => parseInt(line));
 
     let input = [1, 2, -3, 0, 3, 4, -2];
-    input = [
-        { index: 0, value: 1 },
-        { index: 1, value: 2 },
-        { index: 2, value: -3 },
-        { index: 3, value: 0 },
-        { index: 4, value: 3 },
-        { index: 5, value: 4 },
-        { index: 6, value: -2 },
-    ]
-    let oldIndex = input.find(item => item.value === 4).index;
-    let newIndex = (oldIndex + 4) % input.length;
+    let oldIndex = input.indexOf(4);
+    let newIndex = (oldIndex + 4 + 1) % input.length;
     console.log(oldIndex, newIndex);
 
     let output = [1, 2, -3, 4, 0, 3, -2];
