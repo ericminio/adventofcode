3a2f8895ae0e6ac55c58c3d39ed79e9b351da0d7
ericminio <eric.mignot@gmail.com>
2022-12-20 18:16:03 -0800

    ttt

--- 2022/day.20/solve.js
+++ 2022/day.20/solve.js
@@ -11,10 +11,8 @@ const solve1 = (file) => {
     input.splice(oldIndex, 1);
     input.splice(newIndex, 0, value);
 
-    let output = input;
-
-    let zero = output.indexOf(0);
-    return [1000, 2000, 3000].map(n => nthAfter(n, output, zero)).reduce(add);
+    let zero = input.indexOf(0);
+    return [1000, 2000, 3000].map(n => nthAfter(n, input, zero)).reduce(add);
 };
 
 const nthAfter = (n, values, start) => values[(n + start) % values.length];
