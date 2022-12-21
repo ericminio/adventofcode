1205547ac07208886d9c3080ac72393524b1fcd6
ericminio <eric.mignot@gmail.com>
2022-12-20 16:16:31 -0800

    ttt

--- 2022/day.20/solve.js
+++ 2022/day.20/solve.js
@@ -3,11 +3,12 @@ const { add, lines } = require('../support');
 const solve1 = (file) => {
     let incoming = lines(file).map(line => parseInt(line));
 
+    let input = [1, 2, -3, 0, 3, 4, -2];
 
-    let values = [1, 2, -3, 4, 0, 3, -2];
+    let output = [1, 2, -3, 4, 0, 3, -2];
 
-    let zero = values.indexOf(0);
-    return [1000, 2000, 3000].map(n => nthAfter(n, values, zero)).reduce(add);
+    let zero = output.indexOf(0);
+    return [1000, 2000, 3000].map(n => nthAfter(n, output, zero)).reduce(add);
 };
 
 const nthAfter = (n, values, start) => values[(n + start) % values.length];
