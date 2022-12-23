23b8528237ca08d0429b455489742e804f49cc4d
ericminio <eric.mignot@gmail.com>
2022-12-23 11:36:13 -0800

    ttt

--- 2022/day.20/solve.js
+++ 2022/day.20/solve.js
@@ -1,3 +1,5 @@
+const { add } = require('../support');
+
 const move = (n, values) => {
     let oldIndex = values.indexOf(n);
     values.splice(oldIndex, 1);
@@ -6,9 +8,16 @@ const move = (n, values) => {
 
     return values;
 };
+const nth = (n, values) => {
+    let index = values.indexOf(0);
+
+    return values[(index + n) % values.length]
+}
 
 const solve1 = (file) => {
     let values = [1, 2, -3, 4, 0, 3, -2];
+
+    // return [1000, 2000, 3000].map(n => nth(n, values)).reduce(add);
     return 3;
 };
 
