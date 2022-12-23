66bd3541ee6f24c31339172ffc4b222841339b64
ericminio <eric.mignot@gmail.com>
2022-12-22 20:12:29 -0800

    ttt

--- 2022/day.20/solve.js
+++ 2022/day.20/solve.js
@@ -7,7 +7,7 @@ const nth = (n, values) => {
 const solve1 = (file) => {
     let mixed = [1, 2, -3, 4, 0, 3, -2];
 
-    return [nth(1000, mixed), -3, 2].reduce(add);
+    return [1000, 2000, 3000].map(n => nth(n, mixed)).reduce(add);
 };
 
 const solve2 = (file) => {
