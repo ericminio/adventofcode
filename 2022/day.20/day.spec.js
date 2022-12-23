aa76041bc35758140c2cc35e85abbee8891490b4
ericminio <eric.mignot@gmail.com>
2022-12-22 20:24:41 -0800

    ttt

--- 2022/day.20/day.spec.js
+++ 2022/day.20/day.spec.js
@@ -53,6 +53,11 @@ describe.only('2022.20', () => {
                 expect(move(1, [1, 2, 3])).
                     to.deep.equal([2, 1, 3]);
             });
+
+            it('works with 2', () => {
+                expect(move(2, [2, 1, 3])).
+                    to.deep.equal([1, 3, 2]);
+            });
         });
     });
 });
