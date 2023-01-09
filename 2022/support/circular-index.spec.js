0a8eecca4c8b9e587b40da1250e2b8a3ea886c94
ericminio <emignot@change.org>
2023-01-09 15:48:29 -0800

    ttt

--- 2022/support/circular-index.spec.js
+++ 2022/support/circular-index.spec.js
@@ -4,26 +4,26 @@ const { circularIndex } = require('./circular-index');
 describe.only('array circular index', () => {
 
     it('works for zero', () => {
-        let list = [1, 2, 3];
+        let array = [1, 2, 3];
 
-        expect(list[circularIndex(0, list)]).to.equal(1);
+        expect(array[circularIndex(0, array)]).to.equal(1);
     });
 
     it('stays in range', () => {
-        let list = [1, 2, 3];
+        let array = [1, 2, 3];
 
-        expect(list[circularIndex(3, list)]).to.equal(1);
+        expect(array[circularIndex(3, array)]).to.equal(1);
     });
 
     it('works for -1', () => {
-        let list = [1, 2, 3];
+        let array = [1, 2, 3];
 
-        expect(list[circularIndex(-1, list)]).to.equal(3);
+        expect(array[circularIndex(-1, array)]).to.equal(3);
     });
 
     it('works for -4', () => {
-        let list = [1, 2, 3];
+        let array = [1, 2, 3];
 
-        expect(list[circularIndex(-4, list)]).to.equal(3);
+        expect(array[circularIndex(-4, array)]).to.equal(3);
     });
 });
