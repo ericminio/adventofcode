06fc6c8c075c267adc3f275376e190b214b582c4
ericminio <emignot@change.org>
2023-01-09 13:30:41 -0800

    ttt

--- 2022/day.20/solve.js
+++ 2022/day.20/solve.js
@@ -20,7 +20,7 @@ const decrypt = (file) => {
 const push = (start, initial, message) => {
     let value = initial[start];
     let offset = pushCount(value, message);
-    let end = endIndex(start, offset, message);
+    let end = endIndex(start, offset, message)
     let direction = Math.sign(end - start);
     for (let position = start; position !== end; position += direction) {
         [message[position], message[position + direction]] = [
