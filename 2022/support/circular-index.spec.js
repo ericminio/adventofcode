5ddbcdf564b16dc6c44f06e45deb5082c90ed08a
ericminio <emignot@change.org>
2023-01-09 15:46:42 -0800

    ttt

---
+++ 2022/support/circular-index.spec.js
@@ -0,0 +1,29 @@
+const { expect } = require('chai');
+const { circularIndex } = require('./circular-index');
+
+describe('array circular index', () => {
+
+    it('works for zero', () => {
+        let list = [1, 2, 3];
+
+        expect(list[circularIndex(0, list)]).to.equal(1);
+    });
+
+    it('works for 3', () => {
+        let list = [1, 2, 3];
+
+        expect(list[circularIndex(3, list)]).to.equal(1);
+    });
+
+    it('works for -1', () => {
+        let list = [1, 2, 3];
+
+        expect(list[circularIndex(-1, list)]).to.equal(3);
+    });
+
+    it('works for -4', () => {
+        let list = [1, 2, 3];
+
+        expect(list[circularIndex(-4, list)]).to.equal(3);
+    });
+});
