5f4431238c52db0847d082520de4cb1673ebf82d
ericminio <emignot@change.org>
2023-01-09 17:10:45 -0800

    ttt

--- 2022/day.20/push.spec.js
+++ 2022/day.20/push.spec.js
@@ -7,6 +7,8 @@ describe.only('index after pushing', () => {
         let list = [1, 2, 3];
         let message = list.slice();
         push(0, list, message);
+
+        expect(message).to.deep.equal([2, 1, 3])
     });
 
 });
