c6a50796d2d489c4ab6ed26a141cdf20a7884271
ericminio <emignot@change.org>
2023-01-09 21:00:32 -0800

    ttt

--- 2022/day.20/finger-prints.js
+++
@@ -1,8 +0,0 @@
-const { circularIndex } = require('../support/circular-index');
-
-const fingerPrints = (message) => {
-    let zero = message.indexOf(0);
-    return [1000, 2000, 3000].map(n => message[circularIndex(zero + n, message.length)]);
-};
-
-module.exports = { fingerPrints };
