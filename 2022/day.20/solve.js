52b8322e7f922830e60cff22c167dca55053ac69
ericminio <emignot@change.org>
2023-01-09 17:06:40 -0800

    ttt

--- 2022/day.20/solve.js
+++ 2022/day.20/solve.js
@@ -2,18 +2,13 @@ const { add, lines } = require('../support');
 const { fingerPrints } = require('./finger-prints');
 const { push } = require('./push');
 
-const decrypt = (file) => {
+const solve1 = (file) => {
     let input = lines(file).map(line => parseInt(line));
     let message = input.slice();
 
     for (let i = 0; i < input.length; i++) {
         push(i, input, message);
     }
-    return message;
-};
-
-const solve1 = (file) => {
-    let message = decrypt(file);
 
     return fingerPrints(message).reduce(add);
 };
