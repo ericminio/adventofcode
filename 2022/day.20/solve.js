cd312945245d377d68daae03948c561b03a3c48f
ericminio <emignot@change.org>
2023-01-09 21:10:52 -0800

    ttt

--- 2022/day.20/solve.js
+++ 2022/day.20/solve.js
@@ -20,8 +20,8 @@ const solve2 = (file) => {
     return checksum(list);
 };
 
-const checksum = (list) => fingerPrints(asArray(list));
-const fingerPrints = (message) => {
+const checksum = (list) => {
+    let message = asArray(list);
     let zero = message.indexOf(0);
     return [1000, 2000, 3000].map(n => message[circularIndex(zero + n, message.length)]).reduce(add);
 };
