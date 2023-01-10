68f60bb43beb910d923d4f005ea290c03a04d0db
ericminio <emignot@change.org>
2023-01-09 16:30:34 -0800

    ttt

--- 2022/day.20/solve.js
+++ 2022/day.20/solve.js
@@ -2,7 +2,7 @@ const { add, lines } = require('../support');
 const { circularIndex } = require('../support/circular-index');
 
 const fingerPrints = (message) => {
-    let zero = message.indexOf(0)
+    let zero = message.indexOf(0);
     return [1000, 2000, 3000].map(n => message[circularIndex(zero + n, message.length)]);
 };
 const decrypt = (file) => {
