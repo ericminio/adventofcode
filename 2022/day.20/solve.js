d601d5ba104d83403b227e68b1c7cdde983f7794
ericminio <emignot@change.org>
2023-01-09 21:13:25 -0800

    ttt

--- 2022/day.20/solve.js
+++ 2022/day.20/solve.js
@@ -28,7 +28,7 @@ const checksum = (list) => {
 const mix = (initial, list) => {
     for (let i = 0; i < initial.length; i++) {
         let value = initial[i];
-        let direction = value > 0 ? +1 : -1;
+        let direction = Math.sign(value);
         let max = value % (initial.length - 1);
         for (let count = 0; count !== max; count += direction) {
             if (value > 0) { pushRight(list, i); } else { pushLeft(list, i); }
