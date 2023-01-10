97ff3d73fbf2ae154d6479100be70f8c3a87389e
ericminio <emignot@change.org>
2023-01-09 20:50:22 -0800

    ttt

--- 2022/day.20/solve.js
+++ 2022/day.20/solve.js
@@ -5,16 +5,10 @@ const { pushCount } = require('./push-count');
 
 const solve1 = (file) => {
     let input = lines(file).map(line => parseInt(line));
-    let list = buildFrom(input);
 
-    for (let i = 0; i < input.length; i++) {
-        let value = input[i];
-        let increment = value > 0 ? +1 : -1;
-        let max = pushCount(value, input.length);
-        for (let count = 0; count !== max; count += increment) {
-            if (value > 0) { pushRight(list, i); } else { pushLeft(list, i); }
-        }
-    }
+    let list = buildFrom(input);
+    mix(input, list);
+
     let message = asArray(list);
     let total = fingerPrints(message).reduce(add);
 
@@ -23,6 +17,17 @@ const solve1 = (file) => {
 
 const solve2 = (file) => {
     return 15;
+};
+
+const mix = (input, list) => {
+    for (let i = 0; i < input.length; i++) {
+        let value = input[i];
+        let increment = value > 0 ? +1 : -1;
+        let max = pushCount(value, input.length);
+        for (let count = 0; count !== max; count += increment) {
+            if (value > 0) { pushRight(list, i); } else { pushLeft(list, i); }
+        }
+    }
 };
 
 
