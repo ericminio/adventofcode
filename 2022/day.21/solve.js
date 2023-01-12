da6f1bc1fd1e6cfe4204aec7a26c5707efc64752
ericminio <emignot@change.org>
2023-01-12 09:08:58 -0800

    ttt

--- 2022/day.21/solve.js
+++ 2022/day.21/solve.js
@@ -1,4 +1,5 @@
 const { lines } = require('../support/index.js');
+const { parseCell } = require('./parser.js');
 
 const solve1 = (file) => {
     let cellDefinitions = lines(file);
@@ -12,24 +13,4 @@ const solve2 = () => {
     return 15;
 };
 
-let pattern = /(.*):\s(.*)/;
-let parseCell = (incoming) => {
-    let data = pattern.exec(incoming).splice(1);
-    let value;
-    try {
-        value = eval(data[1]);
-    }
-    catch {
-        value = NaN;
-    }
-    let cell = {
-        name: data[0],
-        formula: data[1],
-        value,
-    };
-    return cell;
-};
-
-
-
-module.exports = { solve1, solve2, parseCell };
+module.exports = { solve1, solve2 };
