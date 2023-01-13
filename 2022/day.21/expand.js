15d9c291a6c9013995f9ded3b51313fd7681ca6d
ericminio <eric.mignot@gmail.com>
2023-01-12 22:18:59 -0800

    ttt

--- 2022/day.21/expand.js
+++ 2022/day.21/expand.js
@@ -1,7 +1,12 @@
 const expand = (monkey, formulas) => {
     let formula = formulas[monkey];
 
-
+    try {
+        eval(formula);
+    }
+    catch (error) {
+        console.log(error.message);
+    }
 
     return '3 * 3';
 };
