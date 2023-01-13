160cb4e41f0dddd9a2fdce208d9f44791a91a300
ericminio <emignot@change.org>
2023-01-13 13:59:13 -0800

    ttt

--- 2022/day.25/day.spec.js
+++ 2022/day.25/day.spec.js
@@ -3,7 +3,7 @@ const { solve1, solve2 } = require('./solve');
 
 describe.only('2022.25', () => {
 
-    describe('part 1', () => {
+    describe('figuring out the fuel total needed in SNAFU', () => {
 
         it('leverages an example', () => {
             expect(solve1(`${__dirname}/data/example.txt`)).to.equal('2=-1=0');
