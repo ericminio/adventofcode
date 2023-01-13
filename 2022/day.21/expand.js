e5481df2a8c62a2e4787cc0c6e426ab7ee0e2cb6
ericminio <eric.mignot@gmail.com>
2023-01-12 22:59:59 -0800

    ttt

--- 2022/day.21/expand.js
+++ 2022/day.21/expand.js
@@ -2,30 +2,17 @@ const pattern = /^(.*) is not defined$/;
 
 const expand = (monkey, formulas) => {
     let formula = formulas[monkey];
-
-    try {
-        eval(formula);
-    }
-    catch (error) {
-        let [ variable ] = pattern.exec(error.message).splice(1);
-        let next = formulas[variable];
-        formula = formula.replace(new RegExp(variable, 'g'), `(${next})`);
-    }
-    try {
-        eval(formula);
-    }
-    catch (error) {
-        let [ variable ] = pattern.exec(error.message).splice(1);
-        let next = formulas[variable];
-        formula = formula.replace(new RegExp(variable, 'g'), `(${next})`);
-    }
-    try {
-        eval(formula);
-    }
-    catch (error) {
-        let [ variable ] = pattern.exec(error.message).splice(1);
-        let next = formulas[variable];
-        formula = formula.replace(new RegExp(variable, 'g'), `(${next})`);
+    let done = false;
+    while (!done) {
+        try {
+            eval(formula);
+            done = true;
+        }
+        catch (error) {
+            let [ variable ] = pattern.exec(error.message).splice(1);
+            let next = formulas[variable];
+            formula = formula.replace(new RegExp(variable, 'g'), `(${next})`);
+        }
     }
 
     return formula;
