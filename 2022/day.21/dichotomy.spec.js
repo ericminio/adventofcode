7e7223e89ddba27e251496a94f97721e0589ebc0
ericminio <emignot@change.org>
2023-01-13 09:37:04 -0800

    ttt

--- 2022/day.21/dichotomy.spec.js
+++ 2022/day.21/dichotomy.spec.js
@@ -21,4 +21,24 @@ describe.only('dichotomy', () => {
 
         expect(x).to.equal(1000);
     });
+
+    it('can be arythmetic', () => {
+        const target = 5321;
+        const affine = x => x;
+        const jump = (x, jump) => x + jump;
+
+        let x = 1000;
+        let step = 1000;
+        let around = false;
+        while (! around) {
+            let current = affine(x);
+            let next = affine(jump(x, step));
+            around = (current - target) * (next - target) < 0;
+            if (! around) {
+                x = jump(x, step);
+            }
+        }
+
+        expect(x).to.equal(5000);
+    });
 });
