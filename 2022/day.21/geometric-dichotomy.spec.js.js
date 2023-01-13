dabcfcb86193fcf6bf662cf02c3191ebf6cf3b62
ericminio <emignot@change.org>
2023-01-13 09:19:29 -0800

    ttt

--- 2022/day.21/geometric-dichotomy.spec.js.js
+++ 2022/day.21/geometric-dichotomy.spec.js.js
@@ -1,6 +1,6 @@
 describe.only('geometric dichotomy', () => {
 
-    it('works', () => {
+    it('assumes that the solution is a positive integer', () => {
         const constante = () => 5000;
         const affine = (x) => x;
 
