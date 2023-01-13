bfdbbc7f3bd16d50482084fb2ef4e188517df253
ericminio <eric.mignot@gmail.com>
2023-01-12 20:46:14 -0800

    ttt

--- 2022/day.21/compute.js
+++ 2022/day.21/compute.js
@@ -3,14 +3,21 @@ const compute = ({ cells, values }) => {
     let name = cells[1].name;
     let formula = cells[1].formula;
     let needs = variables(formula);
-    let operation;
+    let available = true;
     needs.forEach(variable => {
-        let pattern = new RegExp(variable, 'g');
-        operation = formula.replace(pattern, values[variable]);
+        if (isNaN(values[variable])) {
+            available = false;
+        }
     });
-    let value = eval(operation);
-
-    values[name] =  value;
+    if (available) {
+        let operation;
+        needs.forEach(variable => {
+            let pattern = new RegExp(variable, 'g');
+            operation = formula.replace(pattern, values[variable]);
+        });
+        let value = eval(operation);
+        values[name] =  value;
+    }
 };
 
 let pattern = /(.*)\s.\s(.*)/;
