44588cb5851f4df22a5fddbf43b4ca7579c69245
ericminio <eric.mignot@gmail.com>
2023-01-12 20:35:47 -0800

    ttt

--- 2022/day.21/compute.spec.js
+++ 2022/day.21/compute.spec.js
@@ -26,21 +26,3 @@ describe.only('Compute', () => {
         ]);
     });
 });
-
-const resolve = ({ cells, values }) => {
-
-    let formula = cells[1].formula;
-    let needs = variables(formula);
-    let operation;
-    needs.forEach(variable => {
-        let pattern = new RegExp(variable, 'g');
-        operation = formula.replace(pattern, values[variable]);
-    });
-    let value = eval(operation);
-
-    cells[1].value =  value;
-};
-
-const variables = (formula) => {
-    return [ 'aaa' ];
-};
