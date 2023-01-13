273768fbe84eb9b23d9498d9663d5bcbf73b15a5
ericminio <emignot@change.org>
2023-01-13 11:57:50 -0800

    ttt

--- 2022/day.24/day.spec.js
+++ 2022/day.24/day.spec.js
@@ -3,7 +3,7 @@ const { solve1, solve2 } = require('./solve');
 
 describe('2022.24', () => {
 
-    describe('part 1', () => {
+    describe('figuring out the fewest number of minutes required to avoid the blizzards and reach the goal?', () => {
 
         it('leverages an example', () => {
             expect(solve1(`${__dirname}/data/example.txt`)).to.equal(18);
