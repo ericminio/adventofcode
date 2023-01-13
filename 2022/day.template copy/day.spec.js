b8362e08135e45f0b94799c360709d758c827687
ericminio <emignot@change.org>
2023-01-13 11:17:42 -0800

    ttt

---
+++ 2022/day.template copy/day.spec.js
@@ -0,0 +1,21 @@
+const { expect } = require('chai');
+const { solve1, solve2 } = require('./solve');
+
+describe.only('2022.22', () => {
+
+    describe('part 1', () => {
+
+        it('leverages an example', () => {
+            expect(solve1(`${__dirname}/data/example.txt`)).to.equal(42);
+        });
+
+    });
+
+    describe('part 2', () => {
+
+        it('leverages an example', () => {
+            expect(solve2(`${__dirname}/data/example.txt`)).to.equal(15);
+        });
+
+    });
+});
