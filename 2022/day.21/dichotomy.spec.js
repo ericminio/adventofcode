f8cb6fa4e138582c919f710e8c211c9e8f8a7d08
ericminio <emignot@change.org>
2023-01-13 09:34:44 -0800

    ttt

--- 2022/day.21/dichotomy.spec.js
+++ 2022/day.21/dichotomy.spec.js
@@ -5,16 +5,17 @@ describe.only('dichotomy', () => {
     it('can be geometric', () => {
         const target = 5321;
         const affine = x => x;
-        const geometric = jump => jump * 10;
+        const geometric = (x, jump) => x * jump;
 
         let x = 1;
+        let jump = 10;
         let around = false;
         while (! around) {
             let current = affine(x);
-            let next = affine(geometric(x));
+            let next = affine(geometric(x, jump));
             around = (current - target) * (next - target) < 0;
             if (! around) {
-                x = geometric(x);
+                x = geometric(x, jump);
             }
         }
 
