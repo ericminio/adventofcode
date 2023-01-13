e8a76a695838e6fafcaadceb82980021f11eaa98
ericminio <eric.mignot@gmail.com>
2023-01-12 23:44:42 -0800

    ttt

--- 2022/day.template/day.spec.js
+++ 2022/day.template/day.spec.js
@@ -5,7 +5,7 @@ describe('2022.??', () => {
 
     describe('part 1', () => {
 
-        it('has an example', () => {
+        it('leverages an example', () => {
             expect(solve1(`${__dirname}/data/example.txt`)).to.equal(42);
         });
 
@@ -13,7 +13,7 @@ describe('2022.??', () => {
 
     describe('part 2', () => {
 
-        it('has an example', () => {
+        it('leverages an example', () => {
             expect(solve2(`${__dirname}/data/example.txt`)).to.equal(15);
         });
 
