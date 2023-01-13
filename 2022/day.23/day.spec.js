c06b8d196d5ff0988fd2491a7a6c703dd9a60ca0
ericminio <emignot@change.org>
2023-01-13 11:58:18 -0800

    ttt

--- 2022/day.23/day.spec.js
+++ 2022/day.23/day.spec.js
@@ -3,7 +3,7 @@ const { solve1, solve2 } = require('./solve');
 
 describe('2022.23', () => {
 
-    describe('how many empty ground tiles does contain the smallest rectangle that contains the Elves after 10 rounds?', () => {
+    describe('figuring out how many empty ground tiles does contain the smallest rectangle that contains the Elves after 10 rounds?', () => {
 
         it('leverages an example', () => {
             expect(solve1(`${__dirname}/data/example.txt`)).to.equal(110);
