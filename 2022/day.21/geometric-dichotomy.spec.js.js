4f12efe99772e581c11cd9dd6463973c16f97571
ericminio <emignot@change.org>
2023-01-13 09:27:32 -0800

    ttt

--- 2022/day.21/geometric-dichotomy.spec.js.js
+++ 2022/day.21/geometric-dichotomy.spec.js.js
@@ -1,8 +1,23 @@
+const { expect } = require('chai');
+
 describe.only('geometric dichotomy', () => {
 
     it('assumes that the solution is a positive integer', () => {
-        const constante = () => 5000;
-        const affine = (x) => x;
+        const target = 5000;
+        const affine = x => x;
+        const geometric = jump => jump * 10;
 
+        let x = 1;
+        let around = false;
+        while (! around) {
+            let current = affine(x);
+            let next = affine(geometric(x));
+            around = (current - target) * (next - target) < 0;
+            if (! around) {
+                x = geometric(x);
+            }
+        }
+
+        expect(x).to.equal(1000);
     });
 });
