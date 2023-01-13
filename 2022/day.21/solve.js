81fd8457b3763074962f547b32b0517db2f023ee
ericminio <emignot@change.org>
2023-01-13 10:36:43 -0800

    ttt

--- 2022/day.21/solve.js
+++ 2022/day.21/solve.js
@@ -25,10 +25,10 @@ const solve2 = (file) => {
             all[cell.name] = cell.formula;
             return all;
         }, {});
-    let [ monkey1, monkey2 ] = formulaPattern.exec(formulas['root']).splice(1)
+    const [ monkey1, monkey2 ] = formulaPattern.exec(formulas['root']).splice(1)
         .map(monkey => expand(monkey, formulas));
 
-    let target = eval(monkey2.replace('humn', 0));
+    const target = eval(monkey2.replace('humn', 0));
     const affine = (humn) => eval(monkey1.replace('humn', humn));
 
     let humn = hug({ target, affine, start: 1, step: 10 });
