4bfe122fb5d80c4a83bc16748fcfafbad453f2aa
ericminio <emignot@change.org>
2023-01-13 15:46:19 -0800

    ttt

--- 2022/day.25/unit.spec.js
+++ 2022/day.25/unit.spec.js
@@ -1,6 +1,10 @@
+const { expect } = require('chai');
+
 describe.only('unit transformation', () => {
 
-    it('works', () => {
-        // 8 = 10 -2
+    it('works for 1', () => {
+        expect(toSNAFU(1)).to.equal('1');
     });
 });
+
+const toSNAFU = (number) => '1';
