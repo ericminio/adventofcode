08231a6cf7286eb88367aa87a9a543027ca395ac
ericminio <eric.mignot@gmail.com>
2023-01-12 21:22:23 -0800

    ttt

--- 2022/day.21/solve.js
+++ 2022/day.21/solve.js
@@ -18,21 +18,21 @@ const solve2 = (file) => {
     let cellDefinitions = lines(file);
 
     tryWith(5, cellDefinitions);
-    let cells = cellDefinitions.map(parseCell);
-    let sheet = { cells, values: {}};
-    cells.forEach(cell => {
-        sheet.values[cell.name] = cell.value;
-    });
-    compute(sheet, 'root');
-
-    console.log(sheet.values['pppw']);
-    console.log(sheet.values['sjmn']);
 
     return 301;
 };
 
 const tryWith = (value, cellDefinitions) => {
+    let cells = cellDefinitions.map(parseCell);
+    let sheet = { cells, values: {}};
+    cells.forEach(cell => {
+        sheet.values[cell.name] = cell.value;
+    });
+    sheet.values['humn'] = value;
+    compute(sheet, 'root');
 
+    console.log(sheet.values['pppw']);
+    console.log(sheet.values['sjmn']);
 };
 
 module.exports = { solve1, solve2 };
