d8104e8fa90fd2786846e27732c6332e0219f8da
ericminio <eric.mignot@gmail.com>
2023-01-12 21:18:38 -0800

    ttt

--- 2022/day.21/solve.js
+++ 2022/day.21/solve.js
@@ -14,7 +14,19 @@ const solve1 = (file) => {
     return sheet.values['root'];
 };
 
-const solve2 = () => {
+const solve2 = (file) => {
+    let cellDefinitions = lines(file);
+
+    let cells = cellDefinitions.map(parseCell);
+    let sheet = { cells, values: {}};
+    cells.forEach(cell => {
+        sheet.values[cell.name] = cell.value;
+    });
+    compute(sheet, 'root');
+
+    console.log(sheet.values['pppw']);
+    console.log(sheet.values['sjmn']);
+
     return 301;
 };
 
