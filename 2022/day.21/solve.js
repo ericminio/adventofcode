0956a3fe2bf06a4a318211328a70991e3c1e5a00
ericminio <eric.mignot@gmail.com>
2023-01-12 23:30:16 -0800

    ttt

--- 2022/day.21/solve.js
+++ 2022/day.21/solve.js
@@ -39,9 +39,7 @@ const solve2 = (file) => {
     while (! around) {
         let first = affine(humn);
         let second = affine(humn + 10);
-        console.log(first, second);
         around = (first - target) * (second - target) < 0;
-        console.log(around);
         if (! around) {
             humn += 10;
         }
