dd9a3730fce009e99575ff93494f11c3ec0583dd
ericminio <emignot@change.org>
2023-01-13 16:23:36 -0800

    ttt

--- 2022/day.25/snafu.js
+++ 2022/day.25/snafu.js
@@ -4,22 +4,17 @@ const fromDecimalToSnafu = (number) => {
 
     digit = 1;
     focus = number % Math.pow(5, digit);
-    console.log(focus);
     if (focus == 1) {
         snafu.unshift('1');
-        console.log(focus * Math.pow(5, digit - 1));
         number = number - focus * Math.pow(5, digit - 1);
-        console.log(number);
     }
 
     if (number > 0) {
         digit ++;
         focus = number % Math.pow(5, digit);
-        console.log(focus);
         if (focus == 1) {
             snafu.unshift('1');
-            number -= focus * Math.pow(5, digit - 1);
-            console.log(number);
+            number = number - focus * Math.pow(5, digit - 1);
         }
     }
 
