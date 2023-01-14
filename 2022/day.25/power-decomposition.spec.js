01aac120cd5a7b4587a8cd5b645b0cdd6cc48fd7
ericminio <eric.mignot@gmail.com>
2023-01-13 22:02:50 -0800

    ttt

--- 2022/day.25/power-decomposition.spec.js
+++ 2022/day.25/power-decomposition.spec.js
@@ -1,7 +1,7 @@
 const { expect } = require('chai');
 const { powerDecomposition } = require('./power-decomposition.js');
 
-describe.only('power decomposition', () => {
+describe('power decomposition', () => {
 
     it('works as expected', () => {
         expect(powerDecomposition(5, 33)).to.deep.equal([ 1, 1, 3 ]);
