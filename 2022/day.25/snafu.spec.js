ca313fcc763801d64a890caa7c0a590a8a541bf4
ericminio <eric.mignot@gmail.com>
2023-01-13 21:57:26 -0800

    ttt

--- 2022/day.25/snafu.spec.js
+++ 2022/day.25/snafu.spec.js
@@ -29,7 +29,7 @@ const powDecomposition = (base, number) => {
         max ++;
     }
     let decomposition = [];
-    for (let power = max ; power > 0; power --) {
+    for (let power = max ; power >= 0; power --) {
         let factor = Math.floor(number / Math.pow(base, power));
         decomposition.push(factor);
         number = number - factor * Math.pow(base, power);
