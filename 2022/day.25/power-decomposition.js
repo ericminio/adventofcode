72fdad4fd10d817c2e689b71a2dedce9bfa8a2b3
ericminio <eric.mignot@gmail.com>
2023-01-13 22:01:30 -0800

    ttt

---
+++ 2022/day.25/power-decomposition.js
@@ -0,0 +1,16 @@
+const powerDecomposition = (base, number) => {
+    let max = 1;
+    while (Math.floor(number / Math.pow(max, base)) > 1) {
+        max ++;
+    }
+    let decomposition = [];
+    for (let power = max ; power >= 0; power --) {
+        let factor = Math.floor(number / Math.pow(base, power));
+        decomposition.push(factor);
+        number = number - factor * Math.pow(base, power);
+    }
+
+    return decomposition;
+};
+
+module.exports = { powerDecomposition };
