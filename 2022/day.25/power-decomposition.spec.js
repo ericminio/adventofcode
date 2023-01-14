83830207d22354d7d541a50479aba2ca9d6c5510
ericminio <eric.mignot@gmail.com>
2023-01-13 22:55:22 -0800

    ttt

--- 2022/day.25/power-decomposition.spec.js
+++ 2022/day.25/power-decomposition.spec.js
@@ -7,7 +7,7 @@ describe('power decomposition', () => {
         expect(powerDecomposition(5, 33)).to.deep.equal([ 1, 1, 3 ]);
     });
 
-    it.only('works for 34978907874317', () => {
+    it('works for 34978907874317', () => {
         expect(powerDecomposition(5, 34978907874317)).to.deep.equal(
             [ 1, 4, 0, 4, 1, 0, 4, 3, 3, 0, 0, 4, 0, 3, 4, 3, 4, 2, 3, 2 ]);
     });
