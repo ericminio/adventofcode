4f070e48b79c3c441745e336b3d69b79cde54796
ericminio <emignot@change.org>
2023-01-13 16:08:18 -0800

    ttt

--- 2022/day.25/unit.spec.js
+++ 2022/day.25/unit.spec.js
@@ -1,4 +1,5 @@
 const { expect } = require('chai');
+const { fromDecimalToSnafu, fromSnafuToDecimal } = require('./snafu');
 
 describe.only('unit transformation', () => {
 
@@ -11,32 +12,3 @@ describe.only('unit transformation', () => {
     });
 });
 
-const fromDecimalToSnafu = (number) => {
-    let snafu = [];
-
-    let digit = 1;
-    let focus = number % Math.pow(5, digit);
-
-    snafu.unshift('1');
-
-    return snafu.join('');
-};
-
-const fromSnafuToDecimal = (snafu) => {
-    let decimal = 0;
-    const digits = snafu.split('');
-    for (let i = 0; i < digits.length; i++) {
-        let digit = digits[i];
-        let value = snafuTable[digit];
-        decimal += value * Math.pow(5, digits.length - i - 1);
-    }
-    return decimal;
-};
-
-const snafuTable = {
-    '0': 0,
-    '1': 1,
-    '2': 2,
-    '=': -2,
-    '-': -1,
-};
