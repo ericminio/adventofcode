c6bbfdb0ad4ac0d1c20bdf803c29e25b95c7ce16
ericminio <eric.mignot@gmail.com>
2023-01-13 22:01:38 -0800

    ttt

--- 2022/day.25/snafu.spec.js
+++ 2022/day.25/snafu.spec.js
@@ -15,25 +15,3 @@ describe.only('snafu transformation', () => {
         expect(fromSnafuToDecimal('1121-1110-1=0')).to.equal(314159265);
     });
 });
-
-describe.only('power decomposition', () => {
-
-    it('works as expected', () => {
-        expect(powDecomposition(5, 33)).to.deep.equal([ 1, 1, 3 ]);
-    });
-});
-
-const powDecomposition = (base, number) => {
-    let max = 1;
-    while (Math.floor(number / Math.pow(max, base)) > 1) {
-        max ++;
-    }
-    let decomposition = [];
-    for (let power = max ; power >= 0; power --) {
-        let factor = Math.floor(number / Math.pow(base, power));
-        decomposition.push(factor);
-        number = number - factor * Math.pow(base, power);
-    }
-
-    return decomposition;
-};
