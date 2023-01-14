bc2f1c83436d5af222443faafd5bae1e1f0feeaa
ericminio <eric.mignot@gmail.com>
2023-01-13 22:03:49 -0800

    ttt

--- 2022/day.25/snafu.spec.js
+++ 2022/day.25/snafu.spec.js
@@ -1,11 +1,14 @@
 const { expect } = require('chai');
 const { fromDecimalToSnafu, fromSnafuToDecimal } = require('./snafu');
 
-describe.only('snafu transformation', () => {
+describe.only('from decimal to snafu', () => {
 
     it('works for 1', () => {
         expect(fromDecimalToSnafu(1)).to.equal('1');
     });
+});
+
+describe.only('from snafu to decimal', () => {
 
     it('works for 1=11-2', () => {
         expect(fromSnafuToDecimal('1=11-2')).to.equal(2022);
