cb6ad512e1be4b4d8c6723d0e5ef67be0d2affdd
ericminio <emignot@change.org>
2023-01-16 09:47:02 -0800

    ttt

--- 2022/day.25/snafu.js
+++ 2022/day.25/snafu.js
@@ -20,7 +20,7 @@ const fromDecimalToSnafu = (number) => {
 const fromSnafuToDecimal = (snafu) => {
     return snafu
         .split('')
-        .reduce((acc, digit, index) => acc + snafuTable[digit] * Math.pow(base, snafu.length - index - 1), 0);
+        .reduce((value, digit, index) => value + snafuTable[digit] * Math.pow(base, snafu.length - index - 1), 0);
 };
 
 const snafuTable = {
