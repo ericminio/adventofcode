835a1ea808c14aa95b44b4a36fb8f9f64ba25ed2
ericminio <emignot@change.org>
2023-01-16 11:26:26 -0800

    ttt

--- 2022/day.18/solve.js
+++ 2022/day.18/solve.js
@@ -1,6 +1,11 @@
 const { lines } = require('../support');
+const { parse } = require('./parser');
 
 const solve1 = (file) => {
+    let cube = {};
+    lines(file).map(line => {
+        cube[line] = parse(line);
+    });
 
     return 64;
 };
