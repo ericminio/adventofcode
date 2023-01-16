c306564cf1e21200925d76be39ecf3c00fea0709
ericminio <emignot@change.org>
2023-01-16 10:00:15 -0800

    ttt

--- 2022/day.18/touching.spec.js
+++ 2022/day.18/touching.spec.js
@@ -3,10 +3,10 @@ const { expect } = require('chai');
 describe.only('exploring cube touching', () => {
 
     it('is fun', () => {
-        let total = exposed([
-            { x: 1, y: 1, z: 1 },
-            { x: 2, y: 1, z: 1 },
-        ]);
+        let total = exposed({
+            '1,1,1': 1,
+            '2,1,1': 1,
+        });
         expect(total).to.equal(10);
     });
 });
