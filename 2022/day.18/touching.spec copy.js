f06c30afa81b6c895e61102e62d89972e4fb3820
ericminio <emignot@change.org>
2023-01-16 11:12:24 -0800

    ttt

---
+++ 2022/day.18/touching.spec copy.js
@@ -0,0 +1,46 @@
+const { expect } = require('chai');
+
+describe.only('exploring cube touching', () => {
+
+    it('is fun', () => {
+        let total = exposed({
+            '1,1,1': { neighbours: around({ x: 1, y: 1, z: 1 }) },
+            '2,1,1': { neighbours: around({ x: 2, y: 1, z: 1 }) }
+        });
+        expect(total).to.equal(10);
+    });
+});
+
+const exposed = (cubes) => {
+    let cubeCount = Object.keys(cubes).length;
+
+    return cubeCount * 6 - touching(cubes) ;
+};
+
+const touching = (cubes) => {
+    let total = 0;
+
+    Object.values(cubes).forEach(cube => {
+        cube.neighbours.forEach(neighbour => {
+            if (cubes[id(neighbour)] !== undefined) {
+                total ++;
+            }
+        });
+    });
+
+    return total;
+};
+
+const around = (cube) => {
+    return [
+        { x: cube.x + 1, y: cube.y, z: cube.z },
+        { x: cube.x - 1, y: cube.y, z: cube.z },
+        { x: cube.x, y: cube.y + 1, z: cube.z },
+        { x: cube.x, y: cube.y - 1, z: cube.z },
+        { x: cube.x, y: cube.y, z: cube.z + 1 },
+        { x: cube.x, y: cube.y, z: cube.z - 1 },
+    ];
+};
+const id = (cube) => {
+    return `${cube.x},${cube.y},${cube.z}`;
+};
