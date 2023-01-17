86be16a1d92982562c703c2c249839f959fc865d
ericminio <emignot@change.org>
2023-01-17 09:50:46 -0800

    ttt

--- 2022/day.18/parser.js
+++ 2022/day.18/parser.js
@@ -3,8 +3,8 @@ const { around } = require('./cube');
 const pattern = /(.*),(.*),(.*)/;
 
 const parse = (line) => {
-    let [ x, y, z ] = pattern.exec(line).splice(1);
-    return { neighbours: around({ x: parseInt(x), y: parseInt(y), z: parseInt(z) }) };
+    let [ x, y, z ] = pattern.exec(line).splice(1).map(entry => parseInt(entry));
+    return { neighbours: around({ x, y, z }) };
 };
 
 module.exports = { parse };
