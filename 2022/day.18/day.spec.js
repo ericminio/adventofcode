7448b3620ec58e757fb15b5cfc01d29f8e1a4ac1
ericminio <emignot@change.org>
2023-01-17 10:28:43 -0800

    ttt

--- 2022/day.18/day.spec.js
+++ 2022/day.18/day.spec.js
@@ -21,9 +21,5 @@ describe.only('2022.18', () => {
             expect(solve2(`${__dirname}/data/example.txt`)).to.equal(58);
         });
 
-        it('is done', () => {
-            expect(solve2(`${__dirname}/data/input.txt`)).to.equal(3316);
-        });
-
     });
 });
