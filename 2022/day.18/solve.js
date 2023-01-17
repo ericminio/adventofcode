ef791f765e7b20635b8b35d298a0afac329a604f
ericminio <emignot@change.org>
2023-01-17 09:50:17 -0800

    ttt

--- 2022/day.18/solve.js
+++ 2022/day.18/solve.js
@@ -1,6 +1,7 @@
 const { lines } = require('../support');
 const { exposed } = require('./exposed');
 const { parse } = require('./parser');
+const { around, id } = require('./cube');
 
 const solve1 = (file) => {
     let cubes = lines(file).reduce((cubes, line) => {
@@ -11,7 +12,17 @@ const solve1 = (file) => {
     return exposed(cubes);
 };
 
-const solve2 = () => {
+const solve2 = (file) => {
+    let cubes = lines(file).reduce((cubes, line) => {
+        cubes[line] = parse(line);
+        return cubes;
+    }, {});
+    // let neighbours = cubes.reduce((neighbours, cube) => {
+    //     cube.neighbours.forEach(neighbour => {
+    //         neighbours[id(neighbour)] = { neighbours: around(neighbours) };
+    //     });
+    //     return neighbours;
+    // }, {});
     return 58;
 };
 
