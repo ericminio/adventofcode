8280393ac30e2fa72c72dcfb4437957c140f4e88
ericminio <eric.mignot@gmail.com>
2023-01-17 23:50:46 -0800

    ttt

---
+++ 2022/day.18/walls.spec copy.js
@@ -0,0 +1,34 @@
+const { expect } = require('chai');
+const { spaceAsHash } = require('./space.js');
+
+const setWall = (id, map) => {
+    Object.keys(map).forEach(key => {
+        let neighbours = map[key].neighbours;
+        let index = neighbours.indexOf(id);
+        if (index !== -1) {
+            neighbours.splice(index, 1);
+        }
+    });
+    delete map[id];
+};
+describe.only('wall', () => {
+
+    let map;
+    beforeEach(() => {
+        map = spaceAsHash({ minimum: { x: 0, y: 0, z: 0 }, maximum: { x: 3, y: 3, z: 3 }});
+    });
+
+    it('is removed from neighbours', () => {
+        setWall('1x0x0', map);
+        expect(map['0x0x0']).to.deep.equal({
+            id: '0x0x0',
+            position: { x: 0, y: 0, z: 0 },
+            neighbours: [ '0x1x0', '0x0x1' ]
+        });
+    });
+
+    it('is removed from space', () => {
+        setWall('1x0x0', map);
+        expect(map['1x0x0']).to.equal(undefined);
+    });
+});
