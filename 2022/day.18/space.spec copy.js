3818faba19a37aa595d6f6d9c3fe7316c2296ec8
ericminio <eric.mignot@gmail.com>
2023-01-17 23:44:48 -0800

    ttt

--- 2022/day.18/space.spec copy.js
+++ 2022/day.18/space.spec copy.js
@@ -1,26 +1,22 @@
 const { expect } = require('chai');
 const { spaceAsHash } = require('./space.js');
 
-describe('map', () => {
+const setWall = (id, map) => {
+    map['0x0x0'].neighbours = [ '0x1x0', '0x0x1' ];
+};
+describe.only('walls', () => {
 
     let map;
     beforeEach(() => {
         map = spaceAsHash({ minimum: { x: 0, y: 0, z: 0 }, maximum: { x: 3, y: 3, z: 3 }});
     });
 
-    it('computes two neighbours as expected', () => {
+    it('is removed from neighbours', () => {
+        setWall('1x0x0', map);
         expect(map['0x0x0']).to.deep.equal({
             id: '0x0x0',
             position: { x: 0, y: 0, z: 0 },
-            neighbours: [ '1x0x0', '0x1x0', '0x0x1' ]
-        });
-    });
-
-    it('computes six neighbours as expected', () => {
-        expect(map['1x1x1']).to.deep.equal({
-            id: '1x1x1',
-            position: { x: 1, y: 1, z: 1 },
-            neighbours: [ '2x1x1', '0x1x1', '1x2x1', '1x0x1', '1x1x2', '1x1x0' ]
+            neighbours: [ '0x1x0', '0x0x1' ]
         });
     });
 });
