ad562b03ffa3a77cd7b4444800b8aa22a7e5b189
ericminio <eric.mignot@gmail.com>
2023-01-17 23:31:16 -0800

    ttt

---
+++ 2022/day.18/boundaries.spec copy.js
@@ -0,0 +1,35 @@
+const { expect } = require('chai');
+const { lines } = require('../support/index.js');
+const { parse } = require('./parser.js');
+const example = `${__dirname}/data/example.txt`;
+
+const boundaries = (positions) => {
+    const xs = positions.map(position => position.x);
+    const ys = positions.map(position => position.y);
+    const zs = positions.map(position => position.z);
+    return {
+        minimum: { x: Math.min(...xs), y: Math.min(...ys), z: Math.min(...zs) },
+        maximum: { x: Math.max(...xs), y: Math.max(...ys), z: Math.max(...zs) }
+    };
+};
+
+describe.only('boundaries', () => {
+
+    let map;
+    beforeEach(() => {
+        let cubes = lines(example).reduce((cubes, line) => {
+            cubes[line] = parse(line);
+            return cubes;
+        }, {});
+        map = boundaries(Object.values(cubes).map(cube => cube.position));
+    });
+
+    it('exposes boundaries', () => {
+        expect(map.minimum).to.deep.equal({ x: 1, y: 1, z: 1 });
+        expect(map.maximum).to.deep.equal({ x: 3, y: 3, z: 6 });
+    });
+
+    it('exposes neighbours', () => {
+
+    });
+});
