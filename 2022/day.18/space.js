186227c1f15d8a38d134b95276682414429084b8
ericminio <eric.mignot@gmail.com>
2023-01-17 23:32:56 -0800

    ttt

--- 2022/day.18/space.js
+++ 2022/day.18/space.js
@@ -38,18 +38,4 @@ const id = (x, y, z) => {
     return `${x}x${y}x${z}`;
 };
 
-describe.only('map', () => {
-
-    let map;
-    beforeEach(() => {
-        map = spaceAsHash({ minimum: { x: 0, y: 0, z: 0 }, maximum: { x: 3, y: 3, z: 3 }});
-    });
-
-    it('computes two neighbours as expected', () => {
-        expect(map['0x0x0']).to.deep.equal({
-            id: '0x0x0',
-            position: { x: 0, y: 0, z: 0 },
-            neighbours: [ '1x0x0', '0x1x0', '0x0x1' ]
-        });
-    });
-});
+module.exports = { spaceAsHash };
