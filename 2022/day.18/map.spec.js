c992073dcc27f3dd8517eb646d7f41f121aded82
ericminio <eric.mignot@gmail.com>
2023-01-17 22:45:25 -0800

    ttt

--- 2022/day.18/map.spec.js
+++ 2022/day.18/map.spec.js
@@ -1,12 +1,14 @@
 const { expect } = require('chai');
 const { solve1 } = require('./solve');
+const example = `${__dirname}/data/example.txt`;
 
 describe.only('map', () => {
 
     describe('boundaries', () => {
 
+
         it('leverages an example', () => {
-            expect(solve1(`${__dirname}/data/example.txt`)).to.equal(64);
+            expect(solve1(example)).to.equal(64);
         });
 
     });
