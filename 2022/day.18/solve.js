5a7d21ae4c5770904372635d1f4b7b98a9e03f85
ericminio <eric.mignot@gmail.com>
2023-01-18 00:08:08 -0800

    ttt

--- 2022/day.18/solve.js
+++ 2022/day.18/solve.js
@@ -44,10 +44,10 @@ const solve2 = (file) => {
     candidates.forEach(candidate => {
         request.target = { id: candidate.id };
         try {
+            console.log(request);
             gps(request, space);
         }
         catch (error) {
-            console.log(error);
             count ++;
         }
     });
