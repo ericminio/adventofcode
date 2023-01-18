4868413ff0de0abe7a0805ef927cd974494e26b0
ericminio <eric.mignot@gmail.com>
2023-01-17 22:43:02 -0800

    ttt

---
+++ 2022/day.18/day.spec copy.js
@@ -0,0 +1,25 @@
+const { expect } = require('chai');
+const { solve1, solve2 } = require('./solve');
+
+describe.only('2022.18', () => {
+
+    describe('figuring out the surface area of your scanned lava droplet', () => {
+
+        it('leverages an example', () => {
+            expect(solve1(`${__dirname}/data/example.txt`)).to.equal(64);
+        });
+
+        it('is done', () => {
+            expect(solve1(`${__dirname}/data/input.txt`)).to.equal(3466);
+        });
+
+    });
+
+    describe('figuring out the exterior surface area of the lava droplet', () => {
+
+        it('leverages an example', () => {
+            expect(solve2(`${__dirname}/data/example.txt`)).to.equal(58);
+        });
+
+    });
+});
