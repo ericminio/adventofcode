73f21a0032c5811488ed94d7d09b1c5e4e622a00
ericminio <emignot@change.org>
2023-01-25 09:23:39 -0800

    ttt

---
+++ 2022/day.17/winds.spec copy.js
@@ -0,0 +1,28 @@
+const { expect } = require('chai');
+const { Winds, LEFT, RIGHT } = require('./winds');
+
+describe.only('winds', () => {
+
+    it('can be left', () => {
+        const winds = new Winds('<');
+
+        expect(winds.next()).to.deep.equal(LEFT);
+    });
+
+    it('can be right', () => {
+        const winds = new Winds('>');
+
+        expect(winds.next()).to.deep.equal(RIGHT);
+    });
+
+    it('loop', () => {
+        const winds = new Winds('<>');
+
+        let current = winds.next();
+        expect(current).to.deep.equal(LEFT);
+        current = winds.next();
+        expect(current).to.deep.equal(RIGHT);
+        current = winds.next();
+        expect(current).to.deep.equal(LEFT);
+    });
+});
