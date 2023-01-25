54dcb385c4cf39f4dea10181149a6eb3b7f9d5e9
ericminio <emignot@change.org>
2023-01-25 09:25:28 -0800

    ttt

--- 2022/day.17/shapes.spec.js
+++ 2022/day.17/shapes.spec.js
@@ -1,28 +1,7 @@
 const { expect } = require('chai');
-const { Winds, LEFT, RIGHT } = require('./winds');
+const { Shapes } = require('./winds');
 
 describe.only('Shapes', () => {
 
-    it('can be left', () => {
-        const winds = new Winds('<');
 
-        expect(winds.next()).to.deep.equal(LEFT);
-    });
-
-    it('can be right', () => {
-        const winds = new Winds('>');
-
-        expect(winds.next()).to.deep.equal(RIGHT);
-    });
-
-    it('loop', () => {
-        const winds = new Winds('<>');
-
-        let current = winds.next();
-        expect(current).to.deep.equal(LEFT);
-        current = winds.next();
-        expect(current).to.deep.equal(RIGHT);
-        current = winds.next();
-        expect(current).to.deep.equal(LEFT);
-    });
 });
