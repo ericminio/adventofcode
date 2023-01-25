95fb949ca3fa8723b3706cd13c853410932bd076
ericminio <emignot@change.org>
2023-01-25 09:24:28 -0800

    ttt

--- 2022/day.17/shapes.js
+++ 2022/day.17/shapes.js
@@ -1,23 +1,11 @@
-const LEFT = { dx: -1 };
-const RIGHT = { dx: 1 };
-
-class Winds {
+class Shapes {
     constructor(specs) {
-        this.directions = specs.split('').map(spec => {
-            if (spec === '<') {
-                return LEFT;
-            }
-            if (spec === '>') {
-                return RIGHT;
-            }
-        });
-        this.current = -1;
+
     }
 
     next() {
-        this.current = (this.current + 1) % this.directions.length;
-        return this.directions[this.current];
+
     }
 }
 
-module.exports = { Winds, LEFT, RIGHT };
+module.exports = { Shapes };
