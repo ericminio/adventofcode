2176aa4fe58592fc431f4233d70a2a637b824450
ericminio <emignot@change.org>
2023-01-26 11:37:24 -0800

    ttt

--- 2022/day.17/rendering.js
+++ 2022/day.17/rendering.js
@@ -1,5 +1,10 @@
 const render = (tower) => {
-
+    const lines = [];
+    for (let i = 0; i <= tower.height; i++) {
+        let line = '.';
+        lines.push(line);
+    }
+    console.log(lines.join('\n'));
 };
 
 module.exports = { render };
