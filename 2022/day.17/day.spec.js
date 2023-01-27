8050597a9489f2b7d65b59e70106aabfdbc25fa0
ericminio <eric.mignot@gmail.com>
2023-01-26 21:14:02 -0800

    ttt

--- 2022/day.17/day.spec.js
+++ 2022/day.17/day.spec.js
@@ -62,7 +62,13 @@ describe.only('2022.17', () => {
                 { x: 0, y: 0 }, { x: 1, y: 1 },
                 { x: 0, y: 1 }, { x: 1, y: 0 }
             ];
-            const left = points;
+            const left = points
+                .map(point => ({ x: point.x - 1, y: point.y }))
+                .filter(point => !points.some(p => p.x == point.x && p.y == point.y));
+
+            expect(left).to.deep.equal([
+                { x: -1, y: 0 }, { x: -1, y: 1 }
+            ]);
         });
     });
 
