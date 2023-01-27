ac2c6e67f9a8c1854ff111f7c71a47f1bb15a092
ericminio <eric.mignot@gmail.com>
2023-01-26 21:18:33 -0800

    ttt

---
+++ 2022/day.17/left.js
@@ -0,0 +1,7 @@
+const leftOf = (points) => {
+    return points
+        .map(point => ({ x: point.x - 1, y: point.y }))
+        .filter(candidate => !points.some(point => point.x == candidate.x && point.y == candidate.y));
+};
+
+module.exports = { leftOf };
