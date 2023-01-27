5486cc286aa769eabd1d4a765cc850300c0f09e3
ericminio <eric.mignot@gmail.com>
2023-01-26 21:48:27 -0800

    ttt

--- 2022/day.17/day.spec.js
+++ 2022/day.17/day.spec.js
@@ -4,9 +4,9 @@ const { solve1, tic, init } = require('./solve');
 const { Winds } = require('./winds');
 const example = `${__dirname}/data/example.txt`;
 
-describe.only('2022.17', () => {
+describe('2022.17', () => {
 
-    describe('figuring out how many units tall will the tower of rocks be after 2022 rocks have stopped falling', () => {
+    describe.only('figuring out how many units tall will the tower of rocks be after 2022 rocks have stopped falling', () => {
 
         it('leverages an example', () => {
             expect(solve1(example, 2)).to.equal(3);
