56aba253bcbbc21faf2900d5008993916312a9e6
ericminio <emignot@change.org>
2023-01-30 13:51:45 -0800

    ttt

--- 2022/day.17/day.spec.js
+++ 2022/day.17/day.spec.js
@@ -67,9 +67,10 @@ describe.only('2022.17', () => {
         });
     });
 
-    describe.only('loop', () => {
-        let deltas = [ 1, 2, 3 ];
-        let base = { rocks: 2, height: 2 };
+    it.only('loop', () => {
+        const deltas = [ 1, 2, 3 ];
+        const base = { rocks: 2, height: 2 };
+
 
     });
 
