fcc41122d2daae8fde679d269ee4a6ceb7ccbf29
ericminio <eric.mignot@gmail.com>
2023-01-30 21:16:21 -0800

    ttt

--- 2022/day.22/day.spec.js
+++ 2022/day.22/day.spec.js
@@ -1,20 +1,12 @@
 const { expect } = require('chai');
-const { solve1, solve2 } = require('./solve');
+const { solve1 } = require('./solve');
 
-describe('2022.22', () => {
+describe.only('2022.22', () => {
 
     describe('figuring out the final password to pass through the force field', () => {
 
         it('leverages an example', () => {
             expect(solve1(`${__dirname}/data/example.txt`)).to.equal(6032);
-        });
-
-    });
-
-    describe('part 2', () => {
-
-        it('leverages an example', () => {
-            expect(solve2(`${__dirname}/data/example.txt`)).to.equal(15);
         });
 
     });
