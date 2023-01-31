10366ac5677cda3bf397c8294ba838968b7dc349
ericminio <eric.mignot@gmail.com>
2023-01-30 21:49:40 -0800

    ttt

--- 2022/day.22/parse.spec.js
+++ 2022/day.22/parse.spec.js
@@ -2,35 +2,12 @@ const { expect } = require('chai');
 const example = `${__dirname}/data/example.txt`;
 const { id, groups } = require('../support/index.js');
 
-const xy = (x, y) => {
-    return { x: x, y: y };
-};
-
 describe.only('parsing the example', () => {
-
-    it('has expectations', () => {
-        const map = {};
-        map[id(xy(9, 1))] = {
-            location: xy(9, 1),
-            neighbours: [
-                id(xy(10, 1)),
-                id(xy(9, 2)),
-                id(xy(9, 12))
-            ]
-        };
-
-        expect(map['9x1']).to.deep.equal({
-            location: { x: 9, y: 1 },
-            neighbours: [
-                '10x1', '9x2', '9x12'
-            ]
-        });
-    });
 
     it('can be explored', () => {
         const map = parse(example);
 
-        // expect(map['9x1']).to.deep.equal({ location: { x: 9, y: 1 }});
+        // expect(map['1x9']).to.deep.equal({ location: { row: 1, column: 9 }});
     });
 });
 
@@ -43,7 +20,7 @@ const parse = (file) => {
         for (let j = 0; j < incoming[i].length; j++) {
             let column = j + 1;
             if (incoming[i][j] === '.') {
-                map[id({ x: row, y: column })] = { location: { x: row, y: column }};
+                map[id({ x: column, y: row })] = { location: { x: column, x: row  }};
             }
         }
     }
