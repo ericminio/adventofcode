64d399e90fede5a973a57596d3d4ef85b18a5cfc
ericminio <emignot@change.org>
2023-01-31 10:22:02 -0800

    ttt

--- 2022/day.22/parse.spec.js
+++ 2022/day.22/parse.spec.js
@@ -9,7 +9,7 @@ describe.only('parsing the example', () => {
     it('can be explored', () => {
         const map = parse(example);
 
-        expect(map.corridors['1x9']).to.deep.equal({
+        expect(map['1x9']).to.deep.equal({
             location: { row: 1, column: 9 },
             neighbours: [ '1x10', '2x9', '12x9' ]
         });
@@ -63,5 +63,5 @@ const parse = (file) => {
             }
         });
     });
-    return map;
+    return map.corridors;
 };
