baebf2d3c6a192f9f917e6d82847d03017e88f14
ericminio <eric.mignot@gmail.com>
2023-01-30 21:25:21 -0800

    ttt

--- 2022/day.22/parse.spec.js
+++ 2022/day.22/parse.spec.js
@@ -1,6 +1,10 @@
+const { id } = require('../support/index.js');
+
 describe.only('parsing the example', () => {
 
     it('has cell 9x1', () => {
-
+        const map = {};
+        const location = { x: 9, y: 1 };
+        map[id(location)] = { location };
     });
 });
