5dcac6bec3e33c8f9fc5628202615ebf23c3cca3
ericminio <eric.mignot@gmail.com>
2023-01-30 21:51:38 -0800

    ttt

--- 2022/day.22/parse.spec.js
+++ 2022/day.22/parse.spec.js
@@ -22,7 +22,7 @@ const parse = (file) => {
         for (let j = 0; j < incoming[i].length; j++) {
             let column = j + 1;
             if (incoming[i][j] === '.') {
-                map[id({ x: column, y: row })] = { location: { x: column, y: row  }};
+                map[id(row, column)] = { location: { row, column }};
             }
         }
     }
