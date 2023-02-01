1c57b05f0f2d01c4f56bd9acec190631f5dd7a07
ericminio <eric.mignot@gmail.com>
2023-01-31 21:50:12 -0800

    ttt

--- 2022/day.22/rotation.js
+++ 2022/day.22/rotation.js
@@ -1,5 +1,5 @@
 const turnLeft = (location) => {
-    return { row: 0 * location.row - 1 * location.column, column: location.row };
+    return { row: 0 * location.row - 1 * location.column, column: 1 * location.row + 0 * location.column };
 };
 
 module.exports = { turnLeft };
