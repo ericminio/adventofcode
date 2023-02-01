b61b11f40601f46364713a564b61870db9efee63
ericminio <eric.mignot@gmail.com>
2023-01-31 21:32:34 -0800

    ttt

--- 2022/day.22/start.js
+++ 2022/day.22/start.js
@@ -1,4 +1,12 @@
+const { ascending, add } = require('../support/index.js');
+
 const start = (map) => {
+    const first = Object.values(map)
+        .filter(cell => cell.location.row == 1)
+        .map(cell => cell.location.column)
+        .sort(ascending)
+        .slice(0, 1)
+        .reduce(add);
     return { row: 1, column: 9 };
 };
 
