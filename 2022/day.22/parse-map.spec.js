a4d4a2475399e54cb573ee146d11c1da23aa2d72
ericminio <eric.mignot@gmail.com>
2023-02-01 06:36:01 -0800

    ttt

--- 2022/day.22/parse-map.spec.js
+++ 2022/day.22/parse-map.spec.js
@@ -12,4 +12,13 @@ describe.only('parsing the map', () => {
             neighbours: [ '1x10', '2x9', '12x9' ]
         });
     });
+
+    it('works for 6x12', () => {
+        const map = parseMap(example);
+
+        expect(map['6x12']).to.deep.equal({
+            location: { row: 6, column: 12 },
+            neighbours: [ '6x1', '7x12', '6x11' ]
+        });
+    });
 });
