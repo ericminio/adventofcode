71e6a616a27c8def9b25464bd59afcf151e2633d
ericminio <eric.mignot@gmail.com>
2023-01-31 21:57:19 -0800

    ttt

--- 2022/day.22/rotation.spec.js
+++ 2022/day.22/rotation.spec.js
@@ -13,5 +13,6 @@ describe.only('rotation', () => {
     it('can be anticlockwise', () => {
         expect(turnRight({ row: 0, column: 1 })).to.deep.equal({ row: 1, column: 0 });
         expect(turnRight({ row: 1, column: 0 })).to.deep.equal({ row: 0, column: -1 });
+        expect(turnRight({ row: 0, column: -1 })).to.deep.equal({ row: -1, column: -0 });
     });
 });
