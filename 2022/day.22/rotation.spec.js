d85cd1c7b8f6186c84e8900f8a510c724dc1c586
ericminio <eric.mignot@gmail.com>
2023-01-31 21:46:39 -0800

    ttt

--- 2022/day.22/rotation.spec.js
+++ 2022/day.22/rotation.spec.js
@@ -1,6 +1,9 @@
+const { expect } = require('chai');
+const { turnLeft } = require('./rotation.js');
+
 describe.only('rotation', () => {
 
     it('can be clockwise', () => {
-
+        expect(turnLeft({ row: 0, column: 1 })).to.deep.equal({ row: -1, column: 0 });
     });
 });
