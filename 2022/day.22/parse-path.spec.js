ff8488506638e57a805a6fee4b6da537d777e013
ericminio <eric.mignot@gmail.com>
2023-01-31 21:23:02 -0800

    ttt

--- 2022/day.22/parse-path.spec.js
+++ 2022/day.22/parse-path.spec.js
@@ -1,5 +1,5 @@
 const { expect } = require('chai');
-const { groups } = require('../support/index.js');
+const { parsePath } = require('./parse-path.js');
 const example = `${__dirname}/data/example.txt`;
 
 describe.only('parsing the path', () => {
@@ -24,35 +24,3 @@ describe.only('parsing the path', () => {
         ]);
     });
 });
-
-const parsePath = (file) => {
-    const spec = groups(file)[1][0];
-    const path = [];
-    let move = '';
-    for (let i = 0; i < spec.length; i++) {
-        let char = spec[i];
-        if (char === 'R') {
-            if (move.length > 0) {
-                path.push({ move: parseInt(move, 10) });
-                move = '';
-            }
-            path.push({ rotate: 'R' });
-        }
-        else if (char === 'L') {
-            if (move.length > 0) {
-                path.push({ move: parseInt(move, 10) });
-                move = '';
-            }
-            path.push({ rotate: 'L' });
-        }
-        else {
-            move += char;
-        }
-    }
-    if (move.length > 0) {
-        path.push({ move: parseInt(move, 10) });
-        move = '';
-    }
-
-    return path;
-};
