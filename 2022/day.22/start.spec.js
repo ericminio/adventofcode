3f85d46793be4fef61172c50f10f58d429cb244a
ericminio <eric.mignot@gmail.com>
2023-01-31 21:28:13 -0800

    ttt

--- 2022/day.22/start.spec.js
+++ 2022/day.22/start.spec.js
@@ -1,4 +1,6 @@
+const { expect } = require('chai');
 const { parseMap } = require('./parse-map.js');
+const { start } = require('./start.js');
 
 const example = `${__dirname}/data/example.txt`;
 
@@ -6,5 +8,8 @@ describe('start', () => {
 
     it('is known', () => {
         const map = parseMap(example);
+        const location = start(map);
+
+        expect(location).to.deep.equal({ row: 1, column: 9 });
     });
 });
