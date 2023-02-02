446070ddfef0f086e2080a5866c899cac7a4f1b3
ericminio <emignot@change.org>
2023-02-02 15:25:05 -0800

    ttt

--- 2022/day.22/rotation.spec.js
+++ 2022/day.22/rotation.spec.js
@@ -1,7 +1,7 @@
 const { expect } = require('chai');
 const { turnLeft, turnRight } = require('./rotation.js');
 
-describe.only('rotation', () => {
+describe('rotation', () => {
 
     it('can be clockwise', () => {
         expect(turnLeft({ row: 0, column: 1 })).to.deep.equal({ row: -1, column: 0 });
