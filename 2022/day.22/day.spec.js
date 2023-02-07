7298e4f7c3e85c6058e228a632fc0c5dc68a0cd1
ericminio <eric.mignot@gmail.com>
2023-02-06 19:22:13 -0800

    ttt

--- 2022/day.22/day.spec.js
+++ 2022/day.22/day.spec.js
@@ -9,7 +9,7 @@ describe.only('2022.22', () => {
             expect(solve1(`${__dirname}/data/example.txt`)).to.equal(6032);
         });
 
-        it('is done', () => {
+        it.only('is done', () => {
             expect(solve1(`${__dirname}/data/input.txt`)).to.equal(1428);
         });
 
