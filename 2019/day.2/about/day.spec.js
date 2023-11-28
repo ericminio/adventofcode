ae950554df11c470b329b8d2389f03e12d16a0ed
Eric Mignot <eric.mignot@gmail.com>
2023-11-28 15:32:00 -0800

    2019.2

--- 2019/day.2/about/day.spec.js
+++ 2019/day.2/about/day.spec.js
@@ -11,4 +11,12 @@ describe('2019.2 puzzles', () => {
             );
         });
     });
+
+    describe('What value is left at position 0 after the program halts?', () => {
+        it('Find the input noun and verb that cause the program to produce the output 19690720. What is 100 * noun + verb?', () => {
+            expect(solveparttwo(input(`${__dirname}/incoming.txt`))).to.equal(
+                9074
+            );
+        });
+    });
 });
--- 2019/day.2/solution/index.js
+++ 2019/day.2/solution/index.js
@@ -8,5 +8,19 @@ module.exports = {
         const values = program.run();
         return values[0];
     },
-    solveparttwo: () => '?',
+    solveparttwo: (input) => {
+        const stop = 19690720;
+        for (let noun = 0; noun < 99; noun++) {
+            for (let verb = 0; verb < 99; verb++) {
+                const program = new Program(input);
+                program.values[1] = noun;
+                program.values[2] = verb;
+                const values = program.run();
+                if (values[0] === stop) {
+                    return 100 * noun + verb;
+                }
+            }
+        }
+        throw new Error('not found');
+    },
 };
