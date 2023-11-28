ba65ccc25d335a351bfbbdd72810f467e2f3ea0e
Eric Mignot <eric.mignot@gmail.com>
2023-11-28 12:57:57 -0800

    2019.1

--- 2019/day.1/about/day.spec.js
+++ 2019/day.1/about/day.spec.js
@@ -11,4 +11,12 @@ describe('2019.1 puzzles', () => {
             );
         });
     });
+
+    describe('What is the sum of the fuel requirements for all of the modules on your spacecraft when also taking into account the mass of the added fuel?', () => {
+        it('is solved', () => {
+            expect(solveparttwo(numbers(`${__dirname}/incoming.txt`))).to.equal(
+                4845669
+            );
+        });
+    });
 });
--- 2019/day.1/solution/index.js
+++ 2019/day.1/solution/index.js
@@ -1,5 +1,15 @@
+const fuel = (mass) => Math.floor(mass / 3) - 2;
+
 module.exports = {
     solvepartone: (masses) =>
-        masses.reduce((total, mass) => total + Math.floor(mass / 3) - 2, 0),
-    solveparttwo: () => '?',
+        masses.reduce((total, mass) => total + fuel(mass), 0),
+    solveparttwo: (masses) =>
+        masses.reduce((total, mass) => {
+            let moduleFuel = fuel(mass);
+            while (moduleFuel > 0) {
+                total += moduleFuel;
+                moduleFuel = fuel(moduleFuel);
+            }
+            return total;
+        }, 0),
 };
