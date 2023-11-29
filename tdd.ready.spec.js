9e8ea7265ba8240e69087dfaedc8713b4bea207a
ericminio <eric.mignot@gmail.com>
2023-11-28 17:09:00 -0800

    prettier

--- tdd.ready.spec.js
+++ tdd.ready.spec.js
@@ -1,21 +1,21 @@
-const expect = require("chai").expect;
+const expect = require('chai').expect;
 
-describe("Testing environment", function () {
-    it("is ready to use chai with mocha", function () {
+describe('Testing environment', function () {
+    it('is ready to use chai with mocha', function () {
         expect(1 + 1).to.equal(2);
     });
 
-    it("is ready to connect to adventofcode", function (done) {
-        var request = require("request");
-        var credentialsFor = require("./me");
-        var url = "http://adventofcode.com/2015/day/1/input";
+    it('is ready to connect to adventofcode', function (done) {
+        var request = require('request');
+        var credentialsFor = require('./me');
+        var url = 'http://adventofcode.com/2015/day/1/input';
 
         request(
             { url: url, jar: credentialsFor(url) },
             function (error, response, input) {
                 expect(response.statusCode).to.equal(200);
                 done();
-            }
+            },
         );
     });
 });
