c1e8b5fa700407b0217bb895a758b18784d97ce7
ericminio <eric.mignot@gmail.com>
2023-11-29 21:56:54 -0800

    repetition matcher

--- 2015/day.5/about/internals/repetition.spec.js
+++ 2015/day.5/about/internals/repetition.spec.js
@@ -5,4 +5,20 @@ describe('repetition matchers', () => {
     it('is happy with efe', () => {
         expect(repetition('efe')).to.equal(true);
     });
+
+    it('is not happy with ee', () => {
+        expect(repetition('ee')).to.equal(false);
+    });
+
+    it('is happy with qjhvhtzxzqqjkmpb', () => {
+        expect(repetition('qjhvhtzxzqqjkmpb')).to.equal(true);
+    });
+
+    it('is happy with xxyxx', () => {
+        expect(repetition('xxyxx')).to.equal(true);
+    });
+
+    it('is not happy with uurcxstgmygtbstg', () => {
+        expect(repetition('uurcxstgmygtbstg')).to.equal(false);
+    });
 });
--- 2015/day.5/solution/repetition.js
+++ 2015/day.5/solution/repetition.js
@@ -1,3 +1,10 @@
 module.exports = (line) => {
-    return true;
+    for (let i = 0; i < line.length; i++) {
+        if (i > 1) {
+            if (line[i] === line[i - 2]) {
+                return true;
+            }
+        }
+    }
+    return false;
 };
