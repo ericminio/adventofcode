e50d2874703e31d6a6d28027eafc1adcb39435a8
ericminio <eric.mignot@gmail.com>
2023-11-30 22:34:28 -0800

    regexp

--- 2023/day.1/solution/value.js
+++ 2023/day.1/solution/value.js
@@ -1,17 +1,2 @@
-module.exports = (line) => {
-    let value = 0;
-    for (let i = 0; i < line.length; i++) {
-        if (!Number.isNaN(parseInt(line[i]))) {
-            value += 10 * parseInt(line[i]);
-            break;
-        }
-    }
-    for (let i = line.length - 1; i >= 0; i--) {
-        if (!Number.isNaN(parseInt(line[i]))) {
-            value += parseInt(line[i]);
-            break;
-        }
-    }
-
-    return value;
-};
+module.exports = (line) =>
+    10 * parseInt(/(\d)/.exec(line)[1]) + parseInt(/.*(\d)/.exec(line)[1]);
