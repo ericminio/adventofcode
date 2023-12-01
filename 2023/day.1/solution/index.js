9f0a786215833f4ab00afee4f838f50a46719aeb
Eric Mignot <eric.mignot@gmail.com>
2023-12-01 11:19:23 -0800

    nicer imports

--- 2023/day.1/solution/index.js
+++ 2023/day.1/solution/index.js
@@ -1,6 +1,7 @@
 import { reduce } from '../../../support/index.js';
-import { real } from './real-value.js';
+
 import { naive } from './naive-value.js';
+import { real } from './real-value.js';
 
 export const solvepartone = (lines) => reduce(lines, naive)
 
