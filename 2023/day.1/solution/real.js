cd132bf4d1a452463accb53032895c377bd131fe
ericminio <eric.mignot@gmail.com>
2023-11-30 23:02:26 -0800

    reduce duplication

--- 2023/day.1/solution/real.js
+++ 2023/day.1/solution/real.js
@@ -10,16 +10,15 @@ const numbers = [
     'nine',
 ];
 
+const firstDigit = new RegExp('(\\d|' + numbers.join('|') + ')');
+const lastDigit = new RegExp('.*(\\d|' + numbers.join('|') + ')');
+
 module.exports = (line) => {
-    const first = /(\d|one|two|three|four|five|six|seven|eight|nine)/.exec(
-        line,
-    )[1];
+    const first = firstDigit.exec(line)[1];
     const index = numbers.indexOf(first);
     let value = 10 * (index === -1 ? parseInt(first) : index + 1);
 
-    const last = /.*(\d|one|two|three|four|five|six|seven|eight|nine)/.exec(
-        line,
-    )[1];
+    const last = lastDigit.exec(line)[1];
     const lastIndex = numbers.indexOf(last);
 
     value += lastIndex === -1 ? parseInt(last) : lastIndex + 1;
