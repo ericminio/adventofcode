35760e0549ebe3cd50a6702f2f71b9286b92ceaf
ericminio <eric.mignot@gmail.com>
2023-12-01 21:56:20 -0800

    parser

---
+++ 2023/day.2/about/internals/game.spec.js
@@ -0,0 +1,45 @@
+import { expect } from 'chai';
+import { game } from '../../solution/game.js';
+
+describe.only('game parser', () => {
+    it('can parse a game with 1 draw of 1 red', () => {
+        const line = 'Game 1: 2 red';
+
+        expect(game(line)).to.deep.equal({
+            id: 1,
+            draws: [{ red: 2, green: 0, blue: 0 }],
+        });
+    });
+
+    it('can parse a game with 1 draw of 1 green', () => {
+        const line = 'Game 1: 2 green';
+
+        expect(game(line)).to.deep.equal({
+            id: 1,
+            draws: [{ red: 0, green: 2, blue: 0 }],
+        });
+    });
+
+    it('can parse a game with 1 draw of 1 blue', () => {
+        const line = 'Game 1: 2 blue';
+
+        expect(game(line)).to.deep.equal({
+            id: 1,
+            draws: [{ red: 0, green: 0, blue: 2 }],
+        });
+    });
+
+    it('can parse a bigger game', () => {
+        const line =
+            'Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue';
+
+        expect(game(line)).to.deep.equal({
+            id: 2,
+            draws: [
+                { red: 0, green: 2, blue: 1 },
+                { red: 1, green: 3, blue: 4 },
+                { red: 0, green: 1, blue: 1 },
+            ],
+        });
+    });
+});
--- 2023/day.2/solution/game.js
+++ 2023/day.2/solution/game.js
@@ -1,9 +1,32 @@
-import { extractor } from '../../../support/index.js';
+import { extract } from '../../../support/index.js';
 
 export const game = (line) => {
-    const data = extractor(/Game (.*):/)(line);
-    const id = data[0];
+    const id = extract(/Game (.*):/, line);
+    const draws = line
+        .substring(line.indexOf(':') + 1)
+        .split(';')
+        .map((draw) => {
+            let rgb = {};
+            try {
+                rgb.red = parseInt(extract(/\s(\d*)\sred/, draw));
+            } catch {
+                rgb.red = 0;
+            }
+            try {
+                rgb.green = parseInt(extract(/\s(\d*)\sgreen/, draw));
+            } catch {
+                rgb.green = 0;
+            }
+            try {
+                rgb.blue = parseInt(extract(/\s(\d*)\sblue/, draw));
+            } catch {
+                rgb.blue = 0;
+            }
+            return rgb;
+        });
+
     return {
         id: parseInt(id),
+        draws,
     };
 };
