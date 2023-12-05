74ad3bc43d03463198c250b2bcf27bae8b6bb391
Eric Mignot <eric.mignot@gmail.com>
2023-12-05 09:49:11 -0800

    parsing

--- 2023/day.3/about/internals/parse.spec.js
+++ 2023/day.3/about/internals/parse.spec.js
@@ -3,11 +3,23 @@ import { expect } from 'chai';
 import { parse } from '../../solution/parse.js';
 
 describe.only('parser', () => {
-    it('can parse one number', () => {
-        expect(parse('467')).to.deep.equal({
-            number: 467,
-            startIndex: 0,
-            endIndex: 2,
-        });
+    it('can parse one number starting the line', () => {
+        expect(parse('467')).to.deep.equal([
+            {
+                number: 467,
+                startIndex: 0,
+                endIndex: 2,
+            },
+        ]);
+    });
+
+    it('can parse one number ending the line', () => {
+        expect(parse('...467')).to.deep.equal([
+            {
+                number: 467,
+                startIndex: 3,
+                endIndex: 5,
+            },
+        ]);
     });
 });
--- 2023/day.3/solution/parse.js
+++ 2023/day.3/solution/parse.js
@@ -1,1 +1,21 @@
-export const parse = (line) => ({ number: 467, startIndex: 0, endIndex: 2 });
+export const parse = (line) => {
+    const numbers = [];
+    let acc = '';
+    let start = 0;
+    for (let i = 0; i < line.length; i++) {
+        const c = line[i];
+        if (!Number.isNaN(parseInt(c))) {
+            if (acc === '') {
+                start = i;
+            }
+            acc += c;
+        }
+    }
+    const number = parseInt(acc);
+    numbers.push({
+        number,
+        startIndex: line.indexOf(acc),
+        endIndex: line.indexOf(acc) + acc.length - 1,
+    });
+    return numbers;
+};
