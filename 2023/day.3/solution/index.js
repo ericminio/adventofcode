c8f850cda4e8c25f285bf6c967496de3d91f378d
Eric Mignot <eric.mignot@gmail.com>
2023-12-05 20:41:34 -0800

    extract more

--- 2023/day.3/solution/index.js
+++ 2023/day.3/solution/index.js
@@ -1,14 +1,15 @@
 import { sumall } from '../../../support/index.js';
+import { isPartNumber } from './isPartNumber.js';
 import { parse } from './parse.js';
-
-export const isPartNumber = (candidate, lines) => {
-    return [467, 35, 633, 617, 592, 755, 664, 598].includes(candidate.number);
-};
 
 export const solvepartone = (lines) => {
     return sumall(
         lines
-            .map((line) => parse(line))
+            .map((line, index) => {
+                const candidate = parse(line);
+                candidate.lineIndex = index;
+                return candidate;
+            })
             .reduce((candidates, numbers) => candidates.concat(numbers))
             .filter((candidate) => isPartNumber(candidate, lines)),
         (part) => part.number,
---
+++ 2023/day.3/solution/isPartNumber.js
@@ -0,0 +1,3 @@
+export const isPartNumber = (candidate, lines) => {
+    return [467, 35, 633, 617, 592, 755, 664, 598].includes(candidate.number);
+};
