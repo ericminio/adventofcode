e01666250c20331bb3786fd649b1387345f26e79
Eric Mignot <eric.mignot@gmail.com>
2023-12-05 20:35:15 -0800

    towards parsing

--- 2023/day.3/solution/index.js
+++ 2023/day.3/solution/index.js
@@ -1,3 +1,25 @@
-export const solvepartone = () => 4361;
+import { sumall } from '../../../support/index.js';
+
+export const isPartNumber = (candidate, lines) => {
+    return [467, 35, 633, 617, 592, 755, 664, 598].includes(candidate.number);
+};
+
+export const solvepartone = (lines) => {
+    return sumall(
+        [
+            { number: 467 },
+            { number: 114 },
+            { number: 35 },
+            { number: 633 },
+            { number: 617 },
+            { number: 58 },
+            { number: 592 },
+            { number: 755 },
+            { number: 664 },
+            { number: 598 },
+        ].filter((candidate) => isPartNumber(candidate, lines)),
+        (part) => part.number,
+    );
+};
 
 export const solveparttwo = () => '?';
