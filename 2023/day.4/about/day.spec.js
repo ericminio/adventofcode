2d2ba00599360b266a51f8ac0c8fd8a6f3c46c33
Eric Mignot <eric.mignot@gmail.com>
2023-12-06 14:30:28 -0800

    2023.4.1

--- 2023/day.4/about/day.spec.js
+++ 2023/day.4/about/day.spec.js
@@ -3,12 +3,18 @@ import { expect } from 'chai';
 import { lines } from '../../../support/index.js';
 import { solvepartone, solveparttwo } from '../solution/index.js';
 
-describe.only('2023.4 puzzles', () => {
+describe('2023.4 puzzles', () => {
     const incoming = lines(new URL('./incoming.txt', import.meta.url));
+
+    describe('How many points are they worth in total?', () => {
+        it('is solved', () => {
+            expect(solvepartone(incoming)).to.equal(24542);
+        });
+    });
 
     it('hs an example for part 1', () => {
         const example = lines(new URL('./example.txt', import.meta.url));
 
-        expect(solvepartone(example)).to.equal(13);
+        expect(solvepartone(example)).to.deep.equal(13);
     });
 });
--- 2023/day.4/solution/index.js
+++ 2023/day.4/solution/index.js
@@ -1,3 +1,25 @@
-export const solvepartone = () => 13;
+import { extractor, sumall } from '../../../support/index.js';
+
+const pattern = /Card\s+(\d+):\s(.*)\s\|\s(.*)/;
+export const nonEmpty = (s) => s.length > 0;
+export const toInt = (s) => parseInt(s);
+export const parse = (cards) => cards.split(' ').filter(nonEmpty).map(toInt);
+
+export const solvepartone = (lines) =>
+    sumall(
+        lines
+            .map((line) => {
+                const [cardId, winning, played] = extractor(pattern)(line);
+                return {
+                    cardId,
+                    winning: parse(winning),
+                    played: parse(played),
+                };
+            })
+            .map((game) => game.played.filter((c) => game.winning.includes(c)))
+            .filter(nonEmpty)
+            .map((cards) => cards.length)
+            .map((size) => Math.pow(2, size - 1)),
+    );
 
 export const solveparttwo = () => '?';
