57a9a22db56f8e32b41a06566af3efbf782c9594
Eric Mignot <eric.mignot@gmail.com>
2023-12-05 20:38:20 -0800

    no dead code

--- 2023/day.3/solution/index.js
+++ 2023/day.3/solution/index.js
@@ -1,4 +1,5 @@
 import { sumall } from '../../../support/index.js';
+import { parse } from './parse.js';
 
 export const isPartNumber = (candidate, lines) => {
     return [467, 35, 633, 617, 592, 755, 664, 598].includes(candidate.number);
@@ -6,18 +7,10 @@ export const isPartNumber = (candidate, lines) => {
 
 export const solvepartone = (lines) => {
     return sumall(
-        [
-            { number: 467 },
-            { number: 114 },
-            { number: 35 },
-            { number: 633 },
-            { number: 617 },
-            { number: 58 },
-            { number: 592 },
-            { number: 755 },
-            { number: 664 },
-            { number: 598 },
-        ].filter((candidate) => isPartNumber(candidate, lines)),
+        lines
+            .map((line) => parse(line))
+            .reduce((candidates, numbers) => candidates.concat(numbers))
+            .filter((candidate) => isPartNumber(candidate, lines)),
         (part) => part.number,
     );
 };
