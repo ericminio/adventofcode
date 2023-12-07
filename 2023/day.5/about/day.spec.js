de8b439c734a8c793a9eab5f749931516d867b5f
Eric Mignot <eric.mignot@gmail.com>
2023-12-06 16:29:04 -0800

    2023.5.1 example magic number

--- 2023/day.5/about/day.spec.js
+++ 2023/day.5/about/day.spec.js
@@ -12,7 +12,7 @@ describe('2023.5 puzzles', () => {
         });
     });
 
-    it('hs an example for part 1', () => {
+    it.only('has an example for part 1', () => {
         const example = lines(new URL('./example.txt', import.meta.url));
 
         expect(solvepartone(example)).to.deep.equal(35);
