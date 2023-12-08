fad59d843c9627882c31c27836216d368fa9741e
Eric Mignot <eric.mignot@gmail.com>
2023-12-08 14:22:57 -0800

    new template

--- 2023/day.6/about/day.spec.js
+++ 2023/day.6/about/day.spec.js
@@ -3,10 +3,10 @@ import { expect } from 'chai';
 import { lines } from '../../../support/index.js';
 import { solvepartone, solveparttwo } from '../solution/index.js';
 
-describe('20??.? puzzles', () => {
+describe('2023.6 puzzles', () => {
     const incoming = lines(new URL('./incoming.txt', import.meta.url));
 
-    describe.skip('part 1', () => {
+    describe.skip('What do you get if you multiply number of ways you can beat the record?', () => {
         it('is solved', () => {
             expect(solvepartone(incoming)).to.equal('???');
         });
