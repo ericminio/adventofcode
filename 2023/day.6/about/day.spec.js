39b5669306e72222d45d75d0b51a423f4db5f72b
Eric Mignot <eric.mignot@gmail.com>
2023-12-08 14:57:30 -0800

    2023.6.1

--- 2023/day.6/about/day.spec.js
+++ 2023/day.6/about/day.spec.js
@@ -1,20 +1,28 @@
 import { expect } from 'chai';
 
-import { lines } from '../../../support/index.js';
 import { solvepartone, solveparttwo } from '../solution/index.js';
 
 describe('2023.6 puzzles', () => {
-    const incoming = lines(new URL('./incoming.txt', import.meta.url));
-
-    describe.skip('What do you get if you multiply number of ways you can beat the record?', () => {
+    describe('What do you get if you multiply number of ways you can beat the record?', () => {
         it('is solved', () => {
-            expect(solvepartone(incoming)).to.equal('???');
+            expect(
+                solvepartone([
+                    { time: 46, record: 214 },
+                    { time: 80, record: 1177 },
+                    { time: 78, record: 1402 },
+                    { time: 66, record: 1024 },
+                ]),
+            ).to.equal(512295);
         });
     });
 
     it('has an example for part 1', () => {
-        const example = lines(new URL('./example.txt', import.meta.url));
-
-        expect(solvepartone(example)).to.deep.equal(8);
+        expect(
+            solvepartone([
+                { time: 7, record: 9 },
+                { time: 15, record: 40 },
+                { time: 30, record: 200 },
+            ]),
+        ).to.deep.equal(288);
     });
 });
--- 2023/day.6/solution/index.js
+++ 2023/day.6/solution/index.js
@@ -1,3 +1,16 @@
-export const solvepartone = () => 8;
+export const solvepartone = (races) => {
+    let count = 1;
+    for (const { time, record } of races) {
+        let better = 0;
+        for (let hold = 0; hold <= time; hold++) {
+            const distance = hold * (time - hold);
+            if (distance > record) {
+                better += 1;
+            }
+        }
+        count *= better;
+    }
+    return count;
+};
 
 export const solveparttwo = () => '?';
