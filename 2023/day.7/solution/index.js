0f5e89db0cade1a8de7aabea2004116703827442
Eric Mignot <eric.mignot@gmail.com>
2023-12-08 15:40:55 -0800

    sorted with magic number

--- 2023/day.7/solution/index.js
+++ 2023/day.7/solution/index.js
@@ -1,3 +1,16 @@
-export const solvepartone = () => 6440;
+import { sumall } from '../../../support/index.js';
+
+export const solvepartone = () => {
+    const hands = [
+        { cards: '32T3K', bid: 765 },
+        { cards: 'KTJJT', bid: 220 },
+        { cards: 'KK677', bid: 28 },
+        { cards: 'T55J5', bid: 684 },
+        { cards: 'QQQJA', bid: 483 },
+    ];
+    const total = sumall(hands.map((hand, index) => (index + 1) * hand.bid));
+
+    return total;
+};
 
 export const solveparttwo = () => '?';
