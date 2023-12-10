0edb00f47dbf6ad9589e3fd21b80552886c4e78a
Eric Mignot <eric.mignot@gmail.com>
2023-12-10 14:23:26 -0800

    about 9 and 10

---
+++ 2023/day.10/solution/index.js
@@ -0,0 +1,3 @@
+export const solvepartone = () => 42;
+
+export const solveparttwo = () => '?';
---
+++ 2023/day.9/about/day.spec.js
@@ -0,0 +1,20 @@
+import { expect } from 'chai';
+
+import { lines } from '../../../support/index.js';
+import { solvepartone, solveparttwo } from '../solution/index.js';
+
+describe('20??.? puzzles', () => {
+    const incoming = lines(new URL('./incoming.txt', import.meta.url));
+
+    describe.skip('part 1', () => {
+        it('is solved', () => {
+            expect(solvepartone(incoming)).to.equal('???');
+        });
+    });
+
+    it('has an example for part 1', () => {
+        const example = lines(new URL('./example.txt', import.meta.url));
+
+        expect(solvepartone(example)).to.deep.equal(42);
+    });
+});
