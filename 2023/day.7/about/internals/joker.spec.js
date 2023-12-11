a409d02ef96dfde9dcf26b7c653cc2a44de86ef2
Eric Mignot <eric.mignot@gmail.com>
2023-12-10 20:23:14 -0800

    joker

--- 2023/day.7/about/internals/joker.spec.js
+++ 2023/day.7/about/internals/joker.spec.js
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 
 import { joker } from '../../solution/joker.js';
+import { count } from '../../solution/counter.js';
 
 describe('joker', () => {
     it('does not touch a type 1 when not present', () => {
@@ -9,5 +10,21 @@ describe('joker', () => {
 
     it('upgrades a type 1 when present', () => {
         expect(joker({ A: 1, B: 1, C: 1, D: 1, J: 1 })).to.equal(2);
+    });
+
+    it('does not touch a type 2 when not present', () => {
+        expect(joker({ A: 1, B: 1, C: 1, D: 2 })).to.equal(2);
+    });
+
+    it('upgrades a type 2 when present once', () => {
+        expect(joker({ A: 1, B: 1, C: 2, J: 1 })).to.equal(4);
+    });
+
+    it('upgrades examples as expected when relevant', () => {
+        expect(joker(count('32T3K'))).to.equal(2);
+        expect(joker(count('T55J5'))).to.equal(6);
+        expect(joker(count('KK677'))).to.equal(3);
+        expect(joker(count('KTJJT'))).to.equal(6);
+        expect(joker(count('QQQJA'))).to.equal(6);
     });
 });
--- 2023/day.7/solution/joker.js
+++ 2023/day.7/solution/joker.js
@@ -1,8 +1,20 @@
 import { type } from './typer.js';
 
 export const joker = (distribution) => {
-    const jokerCount = distribution['J'];
-    if (!jokerCount) return type(distribution);
+    const adjusted = { ...distribution };
+    const jokerCount = adjusted['J'];
+    if (!jokerCount) return type(adjusted);
 
-    return 2;
+    delete adjusted['J'];
+    let keyWithMaxValue;
+    let maxValue = 0;
+    Object.keys(adjusted).forEach((key) => {
+        if (adjusted[key] > maxValue) {
+            maxValue = adjusted[key];
+            keyWithMaxValue = key;
+        }
+    });
+    adjusted[keyWithMaxValue] += jokerCount;
+
+    return type(adjusted);
 };
