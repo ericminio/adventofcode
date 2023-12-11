d5381956792da0ee43445a44dc567b8ccbe34ae6
Eric Mignot <eric.mignot@gmail.com>
2023-12-11 17:39:58 -0500

    about west

--- 2023/day.10/about/internals/connected.spec.js
+++ 2023/day.10/about/internals/connected.spec.js
@@ -1,37 +1,67 @@
 import { expect } from 'chai';
 
-import { around, east } from '../../solution/around.js';
+import { around, east, west } from '../../solution/around.js';
 import { parse } from '../../solution/parser.js';
 import { connected } from '../../solution/connected.js';
 
 describe('connected', () => {
-    it('can identify one connection east going straight', () => {
-        const maze = parse(['S-', '..']);
-        const positions = around({ x: 0, y: 0 }, maze);
-        const candidates = connected(positions, maze);
+    describe('east', () => {
+        it('is not a connection by default', () => {
+            const maze = parse(['S.']);
+            const positions = around({ x: 0, y: 0 }, maze);
+            const candidates = connected(positions, maze);
 
-        expect(candidates).to.deep.equal([
-            { id: '1x0', x: 1, y: 0, direction: east },
-        ]);
+            expect(candidates).to.deep.equal([]);
+        });
+
+        it('can identify one connection east going straight', () => {
+            const maze = parse(['S-']);
+            const positions = around({ x: 0, y: 0 }, maze);
+            const candidates = connected(positions, maze);
+
+            expect(candidates).to.deep.equal([
+                { id: '1x0', x: 1, y: 0, direction: east },
+            ]);
+        });
+
+        it('can identify one connection east going south', () => {
+            const maze = parse(['S7']);
+            const positions = around({ x: 0, y: 0 }, maze);
+            const candidates = connected(positions, maze);
+
+            expect(candidates).to.deep.equal([
+                { id: '1x0', x: 1, y: 0, direction: east },
+            ]);
+        });
+
+        it('can identify one connection east going north', () => {
+            const maze = parse(['SJ']);
+            const positions = around({ x: 0, y: 0 }, maze);
+            const candidates = connected(positions, maze);
+
+            expect(candidates).to.deep.equal([
+                { id: '1x0', x: 1, y: 0, direction: east },
+            ]);
+        });
     });
 
-    it('can identify one connection east going south', () => {
-        const maze = parse(['S7', '..']);
-        const positions = around({ x: 0, y: 0 }, maze);
-        const candidates = connected(positions, maze);
+    describe('west', () => {
+        it('is not a connection by default', () => {
+            const maze = parse(['.S']);
+            const positions = around({ x: 1, y: 0 }, maze);
+            const candidates = connected(positions, maze);
 
-        expect(candidates).to.deep.equal([
-            { id: '1x0', x: 1, y: 0, direction: east },
-        ]);
-    });
+            expect(candidates).to.deep.equal([]);
+        });
 
-    it('can identify one connection east going north', () => {
-        const maze = parse(['SJ', '..']);
-        const positions = around({ x: 0, y: 0 }, maze);
-        const candidates = connected(positions, maze);
+        it('can identify one connection west going straight', () => {
+            const maze = parse(['-S']);
+            const positions = around({ x: 1, y: 0 }, maze);
+            const candidates = connected(positions, maze);
 
-        expect(candidates).to.deep.equal([
-            { id: '1x0', x: 1, y: 0, direction: east },
-        ]);
+            expect(candidates).to.deep.equal([
+                { id: '0x0', x: 0, y: 0, direction: west },
+            ]);
+        });
     });
 });
--- 2023/day.10/solution/connected.js
+++ 2023/day.10/solution/connected.js
@@ -1,9 +1,12 @@
-import { east } from './around.js';
+import { east, west } from './around.js';
 
 export const connected = (around, maze) =>
     around.filter((candidate) => {
         if (candidate.direction === east) {
             return ['-', '7', 'J'].includes(maze[candidate.id].value);
+        }
+        if (candidate.direction === west) {
+            return ['-'].includes(maze[candidate.id].value);
         }
         return false;
     });
