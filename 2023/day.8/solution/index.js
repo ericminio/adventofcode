85662fd866c377e34f631b13cf6a531044d28b2a
Eric Mignot <eric.mignot@gmail.com>
2023-12-10 20:55:08 -0800

    2023.8.1 example

--- 2023/day.8/solution/index.js
+++ 2023/day.8/solution/index.js
@@ -1,16 +1,25 @@
 import { extractor } from '../../../support/index.js';
 
+const pattern = /(.*) = \((.*), (.*)\)/;
+
 export const solvepartone = (lines) => {
-    const lr = lines[0];
+    const lrs = lines[0];
     const tree = {};
     lines.slice(1).forEach((line) => {
-        const data = extractor(/(.*) = \((.*), (.*)\)/)(line);
+        const data = extractor(pattern)(line);
         tree[data[0]] = { L: data[1], R: data[2] };
     });
 
-    console.log(lr, tree);
+    const start = extractor(pattern)(lines[1])[0];
+    let current = start;
+    let step = 0;
+    while (current !== 'ZZZ') {
+        step += 1;
+        const lr = lrs[(step - 1) % lrs.length];
+        current = tree[current][lr];
+    }
 
-    return 6;
+    return step;
 };
 
 export const solveparttwo = () => '?';
