5991a158b6211556e62a01c4832854405d8f7407
Eric Mignot <eric.mignot@gmail.com>
2023-12-10 20:42:22 -0800

    parsing

--- 2023/day.8/solution/index.js
+++ 2023/day.8/solution/index.js
@@ -1,3 +1,16 @@
-export const solvepartone = () => 6;
+import { extractor } from '../../../support/index.js';
+
+export const solvepartone = (lines) => {
+    const lr = lines[0];
+    const tree = {};
+    lines.slice(1).forEach((line) => {
+        const data = extractor(/(.*) = \((.*), (.*)\)/)(line);
+        tree[data[0]] = { L: data[1], R: data[2] };
+    });
+
+    console.log(lr, tree);
+
+    return 6;
+};
 
 export const solveparttwo = () => '?';
