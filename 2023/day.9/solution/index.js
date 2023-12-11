98ab97cf00060180487e3627a73186abe6e56366
Eric Mignot <eric.mignot@gmail.com>
2023-12-11 00:48:23 -0800

    explore

--- 2023/day.9/solution/index.js
+++ 2023/day.9/solution/index.js
@@ -1,4 +1,21 @@
-export const solvepartone = () => {
+export const solvepartone = (lines) => {
+    for (const line of lines) {
+        const numbers = line.split(' ').map((e) => parseInt(e));
+
+        let pyramid = [numbers.slice()];
+        let current = pyramid[pyramid.length - 1];
+        while (current.some((e) => e !== 0)) {
+            let next = [];
+            for (let i = 1; i < current.length; i++) {
+                next.push(current[i] - current[i - 1]);
+            }
+            pyramid.push(next);
+            current = pyramid[pyramid.length - 1];
+        }
+
+        console.log(pyramid);
+    }
+
     return 114;
 };
 
