8c8efb14f09c3b2c5182c13c7b7955f1d564ae92
Eric Mignot <eric.mignot@gmail.com>
2023-12-20 11:13:19 -0800

    pretend we assign a zipcode to a single cluster

--- zipcodes/about/clusters.spec.js
+++ zipcodes/about/clusters.spec.js
@@ -2,14 +2,32 @@ import { expect } from 'chai';
 
 import { input } from '../../support/index.js';
 import { solvepartone, solveparttwo } from '../solution/index.js';
+import { parse } from '../solution/parser.js';
+import { clusters } from '../solution/clusters.js';
 
-describe('zipcode puzzles', () => {
-    it('has an example', () => {
+describe('clusters', () => {
+    it('can be identified', () => {
         const example = input(new URL('./example.txt', import.meta.url));
 
         expect(solvepartone(example)).to.deep.equal([
             { count: 5, contributors: ['AAAAA', 'BBBBB'] },
             { count: 5, contributors: ['EEEEE'] },
+        ]);
+    });
+
+    it('assign a zipcode to a single cluster', () => {
+        const example = input(
+            new URL('./example-in-between.txt', import.meta.url),
+        );
+        const incoming = parse(example);
+        const actual = clusters(incoming, {
+            maxDistanceToBeInCluster: 5,
+            minSignaturePercentageToBeACluster: 0.2,
+        });
+        console.log(actual);
+        expect(actual).to.deep.equal([
+            { count: 2, contributors: ['AAAAA', 'BBBBB'] },
+            { count: 1, contributors: ['CCCCC'] },
         ]);
     });
 });
